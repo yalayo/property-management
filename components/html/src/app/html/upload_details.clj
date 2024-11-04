561c6e925eabbf6d8b85a10b6a922dd275a86116
Yunior Alayo <yunior.alayo@gmail.com>
2024-11-04 11:06:35 +0100

    Small label update.

--- components/html/src/app/html/upload_details.clj
+++ components/html/src/app/html/upload_details.clj
@@ -9,7 +9,7 @@
      {:class "mx-auto max-w-2xl"}
      [:div
       {:class "text-center"}
-      [:h1 {:class "text-balance text-4xl font-bold tracking-tight text-gray-900 sm:text-6xl"} "Kopfdaten importieren"]
+      [:h1 {:class "text-balance text-4xl font-bold tracking-tight text-gray-900 sm:text-6xl"} "Kalkulationen importieren"]
       [:div {:id "upload-file" :class "mt-10 flex items-center justify-center gap-x-6"}
        [:form
         {:hx-encoding "multipart/form-data"
