63d0a47b8f5a890230895d1d1a7ed75d3ed7f8b6
Yunior Alayo <yunior.alayo@gmail.com>
2024-11-04 10:32:52 +0100

    Temporarily removed the user management. Not needed for this first version.

--- components/web/src/app/web/core.clj
+++ components/web/src/app/web/core.clj
@@ -4,19 +4,19 @@
             [io.pedestal.http.ring-middlewares :as middlewares]
             [jdbc-ring-session.core :as jdbc-ring-session]
             [app.html.interface :as html]
-            [app.user.interface :refer [get-routes get-datasource]]))
+            #_[app.user.interface :refer [get-routes get-datasource]]))
 
-(def session-interceptor
+#_(def session-interceptor
   (middlewares/session {:store (jdbc-ring-session/jdbc-store (get-datasource) {:table :session_store})}))
 
 (def service
   (-> {:env :prod
-       ::http/routes (route/expand-routes (into #{} (concat (get-routes) (html/get-routes))))
+       ::http/routes (route/expand-routes (into #{} (html/get-routes) #_(concat (get-routes) (html/get-routes))))
        ::http/resource-path "/public"
        ::http/type :immutant
        ::http/port 8080}
       (http/default-interceptors)
-      (update ::http/interceptors concat [session-interceptor])
+      (update ::http/interceptors concat [#_session-interceptor])
       http/create-server))
 
 (defn start []
