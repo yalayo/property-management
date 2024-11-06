18534567fe481c69247f3879f5b880022e0e2cb0
Yunior Alayo <yunior.alayo@gmail.com>
2024-11-06 18:09:01 +0100

    Clean up the excel component.

--- components/excel/src/app/excel/core.clj
+++ components/excel/src/app/excel/core.clj
@@ -1,25 +1,9 @@
 (ns app.excel.core
-  (:require [dk.ative.docjure.spreadsheet :as docj])
+  (:require [dk.ative.docjure.spreadsheet :as docj]
+            [clojure.java.io :as io])
   (:import [org.apache.poi.ss.usermodel CellType]))
 
-(defn process-details [input-stream]
-  (->> (docj/load-workbook input-stream)
-       (docj/select-sheet "Price List")
-       (docj/select-columns {:A :name, :B :price})))
-
-(->> (docj/load-workbook-from-resource "test.xlsx")
-     (docj/select-sheet "Tabelle1")
-     (docj/select-columns {:A :name, :B :price}))
-
-(->> (docj/load-workbook-from-resource "test.xlsx")
-     (docj/select-sheet "Tabelle1")
-     (docj/select-cell "A1"))
-
-(-> (docj/load-workbook-from-resource "test.xlsx")
-     (docj/select-name "table1"))
-
-(let [cells (-> (docj/load-workbook-from-resource "test.xlsx")
-                (docj/select-name "table1"))]
+(defn get-content [cells]
   (map (fn [cell]
          (let [cell-type (str (.getCellType cell))]  ; Convert cell type to a string for matching
            (case cell-type
@@ -29,3 +13,16 @@
              "FORMULA" (.getCellFormula cell)
              "BLANK" nil
              (throw (IllegalArgumentException. (str "No matching clause: " cell-type)))))) cells))
+
+(defn process-details [input-stream]
+  (let [workbook (docj/load-workbook input-stream)
+        headers (get-content (-> workbook 
+                                 (docj/select-name "headers")))
+        content (get-content (-> workbook
+                    (docj/select-name "table1")))]
+    {:headers headers
+     :content (partition (count headers) content)}))
+
+(comment
+  (process-details (io/input-stream "D:/personal/projects/inmo-verwaltung/code/property-management/components/excel/resources/test.xlsx"))
+  )
