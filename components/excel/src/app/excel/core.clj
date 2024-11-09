a706987f36ac051968bb8bf90e4eb54692186be9
Yunior Alayo <yunior.alayo@gmail.com>
2024-11-09 11:45:16 +0100

    Added more data extracted from the excel file.

--- components/excel/src/app/excel/core.clj
+++ components/excel/src/app/excel/core.clj
@@ -3,13 +3,24 @@
             [clojure.java.io :as io]
             [clojure.string :as str]))
 
+(defn get-formula-value [cell]
+  (let [workbook (.getWorkbook (.getSheet cell))
+        evaluator (.createFormulaEvaluator (.getCreationHelper workbook))
+        evaluated-cell (.evaluate evaluator cell)
+        cell-type (str (.getCellType evaluated-cell))]
+    (case cell-type
+      "NUMERIC" (.getNumericCellValue cell)
+      "STRING" (.getStringCellValue cell)
+      "BOOLEAN" (.getBooleanCellValue cell)
+      "BLANK" nil)))
+
 (defn get-cell-value [cell]
   (let [cell-type (str (.getCellType cell))]  ; Convert cell type to a string for matching
     (case cell-type
       "NUMERIC" (.getNumericCellValue cell)
       "STRING" (.getStringCellValue cell)
       "BOOLEAN" (.getBooleanCellValue cell)
-      "FORMULA" (.getCellFormula cell)
+      "FORMULA" (get-formula-value cell)
       "BLANK" nil
       (throw (IllegalArgumentException. (str "No matching clause: " cell-type))))))
 
@@ -36,19 +47,29 @@
         filtered (filter #(str/starts-with? (.getSheetName %) "W") sheets)]
     (into [] (map (fn [sheet]
                     (let [last-name (subs (.getSheetName sheet) 3)
-                          street (get-cell-value (docj/select-cell "A2" sheet))]
-                      {:last-name last-name :street street})) filtered))))
+                          street (get-cell-value (docj/select-cell "A2" sheet))
+                          headers (get-content (docj/select-name workbook (str "h" (.getSheetName sheet))))
+                          content (get-content (docj/select-name workbook (str "t" (.getSheetName sheet))))]
+                      {:last-name last-name
+                       :street street
+                       :headers headers
+                       :content (format-content content (count headers))})) filtered))))
 
 (comment
   (process-details (io/input-stream "D:/personal/projects/inmo-verwaltung/code/property-management/components/excel/resources/test.xlsx"))
 
-  (let [input-stream (io/input-stream "D:/personal/projects/inmo-verwaltung/code/property-management/components/excel/resources/NK_ 2023_kuni.xlsx")
+  (let [input-stream (io/input-stream "D:/personal/projects/inmo-verwaltung/code/property-management/components/excel/resources/NK_ 2023_kuni_2.xlsx")
         workbook (docj/load-workbook input-stream)
         sheets (docj/sheet-seq workbook) 
         filtered (filter #(str/starts-with? (.getSheetName %) "W") sheets)]
     (into [] (map (fn [sheet]
            (let [last-name (subs (.getSheetName sheet) 3)
-                 street (get-cell-value (docj/select-cell "A2" sheet))]
-             {:last-name last-name :street street})) filtered)))
+                 street (get-cell-value (docj/select-cell "A2" sheet))
+                 headers (get-content (docj/select-name workbook (str "h" (.getSheetName sheet))))
+                 content (get-content (docj/select-name workbook (str "t" (.getSheetName sheet))))]
+             {:last-name last-name 
+              :street street
+              :headers headers 
+              :content (format-content content (count headers))})) filtered)))
 
 )
