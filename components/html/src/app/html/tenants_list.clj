8ef5068b6934a55d88424a30b54418d018340520
Yunior Alayo <yunior.alayo@gmail.com>
2024-11-09 19:23:05 +0100

    Updated te button to open the pdf in a new tab.

--- components/html/src/app/html/tenants_list.clj
+++ components/html/src/app/html/tenants_list.clj
@@ -20,9 +20,7 @@
      [:button
       {:type "button",
        :class "inline-flex items-center rounded-md bg-indigo-600 px-3 py-2 text-sm font-semibold text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
-       :hx-get (str "/tenants/" (:tenant-id tenant))
-       :hx-target "this"
-       :hx-swap "outerHTML"}
+       :onclick (str "window.open('/tenants/" (:tenant-id tenant) "', '_blank');")}
       [:svg
        {:class "-ml-0.5 mr-1.5 h-5 w-5",
         :viewBox "0 0 20 20",
