ee2b8cdaa15b8574b053792216597a4082ecb685
Yunior Alayo <yunior.alayo@gmail.com>
2024-11-09 11:57:43 +0100

    Small update in the path url.

--- components/html/src/app/html/tenants_list.clj
+++ components/html/src/app/html/tenants_list.clj
@@ -20,7 +20,7 @@
      [:button
       {:type "button",
        :class "inline-flex items-center rounded-md bg-indigo-600 px-3 py-2 text-sm font-semibold text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
-       :hx-get (str "/tenant/" (:tenant-id tenant))
+       :hx-get (str "/tenants/" (:tenant-id tenant))
        :hx-target "this"
        :hx-swap "outerHTML"}
       [:svg
