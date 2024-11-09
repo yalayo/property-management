0915056c21c15aa4b19dd0de0b30191856a5dc75
Yunior Alayo <yunior.alayo@gmail.com>
2024-11-09 11:54:00 +0100

    Use the tenant-id intead of last-name.

--- components/html/src/app/html/tenants_list.clj
+++ components/html/src/app/html/tenants_list.clj
@@ -20,7 +20,7 @@
      [:button
       {:type "button",
        :class "inline-flex items-center rounded-md bg-indigo-600 px-3 py-2 text-sm font-semibold text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
-       :hx-get (str "/tenant/" (:last-name tenant))
+       :hx-get (str "/tenant/" (:tenant-id tenant))
        :hx-target "this"
        :hx-swap "outerHTML"}
       [:svg
@@ -33,7 +33,7 @@
          :d
          "M16.704 4.153a.75.75 0 01.143 1.052l-8 10.5a.75.75 0 01-1.127.075l-4.5-4.5a.75.75 0 011.06-1.06l3.894 3.893 7.48-9.817a.75.75 0 011.05-.143z",
          :clip-rule "evenodd"}]]
-      "Auswählen"]]
+      "Brief"]]
     [:div
      {:class "relative ml-3 sm:hidden"}
      [:button
