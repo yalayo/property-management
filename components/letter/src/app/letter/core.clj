d8ed544d863607086196a2fced66e2163c238c95
Yunior Alayo <yunior.alayo@gmail.com>
2024-11-23 16:31:48 +0100

    Removed a text line.

--- components/letter/src/app/letter/core.clj
+++ components/letter/src/app/letter/core.clj
@@ -102,8 +102,6 @@
         [:paragraph {:size 10 :align :left :spacing-before 10} "Sie schließt mit einer Gutschrift für den 2023 i. H. von " [:phrase {:style :bold} (str (format "%.2f" (:total tenant)) " €")]]
         (payment-information (:total tenant) (:payment-info tenant)))
 
-      [:paragraph {:size 10 :align :left :spacing-before 10} "Bei Rückfragen sind wir gerne behilflich."]
-
       [:paragraph {:size 10 :align :left :spacing-before 20 :spacing-after 30} "Mit freundlichen Grüßen"]
 
       [:paragraph {:size 10 :align :left} "Christian Friese und Rosa Martinez"]] 
