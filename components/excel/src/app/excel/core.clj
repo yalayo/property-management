084bc0be899a10917f22d932335a37446465204e
Yunior Alayo <yunior.alayo@gmail.com>
2024-12-21 20:30:34 +0100

    Refactor to change how errors are detected.

--- components/excel/src/app/excel/core.clj
+++ components/excel/src/app/excel/core.clj
@@ -66,61 +66,56 @@
           (throw (IllegalArgumentException. (str "No matching clause: " cell-type))))
         (catch Exception e {:error true :message (.getMessage e) :cell-address cell-address})))))
 
+(defn get-cell-data [cell name]
+  (when (some? cell)
+    (let [cell-type (str (.getCellType cell))
+          cell-address (.formatAsString (.getAddress cell))]
+      (try
+        (case cell-type
+          "NUMERIC" {name (.getNumericCellValue cell)}
+          "STRING" {name (.getStringCellValue cell)}
+          "BOOLEAN" {name (.getBooleanCellValue cell)}
+          "FORMULA" {name (get-formula-value cell)}
+          "BLANK" nil
+          (throw (IllegalArgumentException. (str "No matching clause: " cell-type))))
+        (catch Exception e {:error true :message (.getMessage e) :cell-address cell-address})))))
+
+(def attributes [{:name "last-name" :cell "B3"}
+                 {:name "property-id" :cell "C3"}
+                 {:name "street" :cell "A2"}
+                 {:name "location" :cell "B2"}
+                 {:name "total-costs" :cell "I2"}
+                 {:name "prepayment" :cell "I3"}
+                 {:name "heating-costs" :cell "I4"}
+                 {:name "total" :cell "I5"}
+                 {:name "iban" :cell "L2"}
+                 {:name "bank-name" :cell "L3"}
+                 {:name "refund?" :cell "I6"}])
+
+(defn get-attribute-value [data]
+  (let [sheet (:sheet data)
+        name (keyword (:name data))
+        cell (:cell data)]
+    (get-cell-data (docj/select-cell cell sheet) name)))
+
 (defn process [input-stream]
   (let [workbook (docj/load-workbook input-stream)
         sheets (docj/sheet-seq workbook)
-        filtered (filter #(str/starts-with? (.getSheetName %) "W") sheets)
-        errors (atom {})] ;; Creamos un mapa para almacenar los errores
-    (into [] (map (fn [sheet]
-                    (let [last-name (get-cell-value (docj/select-cell "B3" sheet))
-                          property-id (get-cell-value (docj/select-cell "C3" sheet))
-                          street (get-cell-value (docj/select-cell "A2" sheet))
-                          location (get-cell-value (docj/select-cell "B2" sheet))
-                          total-costs (get-cell-value (docj/select-cell "I2" sheet))
-                          prepayment (get-cell-value (docj/select-cell "I3" sheet))
-                          heating-costs (get-cell-value (docj/select-cell "I4" sheet))
-                          total (get-cell-value (docj/select-cell "I5" sheet))
-                          iban (get-cell-value (docj/select-cell "L2" sheet))
-                          bank-name (get-cell-value (docj/select-cell "L3" sheet))
-                          refund? (get-cell-value (docj/select-cell "I6" sheet))
-                          headers (get-content (docj/select-name workbook (str "h" (.getSheetName sheet))))
-                          content (get-content (docj/select-name workbook (str "t" (.getSheetName sheet))))]
+        filtered (filter #(str/starts-with? (.getSheetName %) "W") sheets)]
+    (vec (flatten (map (fn [sheet]
+           (let [refund? (get-cell-value (docj/select-cell "I6" sheet))
+                 headers (get-content (docj/select-name workbook (str "h" (.getSheetName sheet))))
+                 content (get-content (docj/select-name workbook (str "t" (.getSheetName sheet))))
+                 data (map #(assoc % :sheet sheet) attributes)
+                 result (map get-attribute-value data)]
+             (if (some #(:error %) result)
+               (filter :error result)
+               result))) filtered))))) 
 
-                      ;; Revisamos si alguna celda tiene error y la agregamos al mapa de errores
-                      (doseq [cell [last-name property-id street location total-costs prepayment heating-costs total iban bank-name refund?]]
-                        (when (and (map? cell) (:error cell))
-                          (swap! errors update (:cell-address cell) conj (:message cell))))
+(comment 
+  (process (io/input-stream "D:/personal/projects/inmo-verwaltung/work-data/for-the-letters/to_validate_wrong_data_in_column.xlsx"))
+  )
 
-                      {:tenant-id (str (java.util.UUID/randomUUID))
-                       :last-name (:message last-name) ;; Esto asume que el mensaje de error está en el :message
-                       :street (:message street)
-                       :location (:message location)
-                       :total-costs (:message total-costs)
-                       :prepayment (:message prepayment)
-                       :heating-costs (:message heating-costs)
-                       :total (:message total)
-                       :payment-info {:iban (:message iban) :bank-name (:message bank-name)}
-                       :refund refund?
-                       :headers (format-headers headers)
-                       :content (format-content content (count headers))
-                       :property-info {:id (:message property-id)
-                                       :name (get-cell-value (docj/select-cell "L5" sheet))
-                                       :address (get-cell-value (docj/select-cell "M5" sheet))
-                                       :apartment (get-cell-value (docj/select-cell "M6" sheet))
-                                       :time-period (get-cell-value (docj/select-cell "M7" sheet))
-                                       :calculated-days (get-cell-value (docj/select-cell "M8" sheet))
-                                       :days-per-person (get-cell-value (docj/select-cell "M9" sheet))}})) filtered)
-    ;; Verificamos si hay errores en el mapa
-    (if (seq @errors)
-      {:status "error"
-       :errors @errors} ;; Si hay errores, los retornamos
-      {:status "success"
-       :data filtered})))) ;; Si no hay errores, seguimos con los datos procesados
-
-
-#_{:error true :message (.getMessage e)}
-#_ (try)
-#_(catch  Exception e (println (.getMessage e)))
 
 (comment
   (process-details (io/input-stream "D:/personal/projects/inmo-verwaltung/code/property-management/components/excel/resources/test.xlsx"))
