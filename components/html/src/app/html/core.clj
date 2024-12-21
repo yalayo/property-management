b95fdc07f3d3ac808c0167f35da9bd6732233f9d
Yunior Alayo <yunior.alayo@gmail.com>
2024-12-21 20:38:05 +0100

    Small change after fixing a conflict.

--- components/html/src/app/html/core.clj
+++ components/html/src/app/html/core.clj
@@ -63,7 +63,6 @@
             (let [multipart-data (:multipart-params (-> context :request))
                   file (get multipart-data "file")
                   file-input-stream (:tempfile file)]
-<<<<<<< HEAD
               (if (some? file-input-stream) 
                 (let [result (excel/process file-input-stream)]
                   (if (some #(:error %) result)
