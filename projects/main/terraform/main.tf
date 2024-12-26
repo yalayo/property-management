a27d76b4d39732905a0b613792cd43b615acb593
Yunior Alayo <yunior.alayo@gmail.com>
2024-12-26 13:12:14 +0100

    Added initial hetzner related config data.

--- projects/main/terraform/main.tf
+++ projects/main/terraform/main.tf
@@ -6,3 +6,16 @@ terraform {
     }
   }
 }
+
+variable "hcloud_token" {
+  sensitive = true
+}
+
+variable "firewall_source_ip" {
+  default = "0.0.0.0"
+}
+
+# Configure the Hetzner Cloud Provider
+provider "hcloud" {
+  token = "${var.hcloud_token}" 
+}
