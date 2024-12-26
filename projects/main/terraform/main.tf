ddc43803d47b9a7d6f1ea67eb988a93a5d706d7e
Yunior Alayo <yunior.alayo@gmail.com>
2024-12-26 13:29:27 +0100

    Added networking and initial vm definition.

--- projects/main/terraform/main.tf
+++ projects/main/terraform/main.tf
@@ -42,3 +42,39 @@ resource "hcloud_firewall" "common-firewall" {
     ]
   }
 }
+
+## Networking
+resource "hcloud_network" "network" {
+  name     = "network"
+  ip_range = "10.0.0.0/16"
+}
+
+resource "hcloud_network_subnet" "network-subnet" {
+  type         = "cloud"
+  network_id   = hcloud_network.network.id
+  network_zone = "eu-central"
+  ip_range     = "10.0.1.0/24"
+}
+
+## VM
+resource "hcloud_server" "immo" { 
+  name        = "prod-immo"
+  image       = "fedora-40"
+  location    = "fsn1"
+  server_type = "cax11" 
+  keep_disk   = true
+  ssh_keys    = ["ssh-key-1"] 
+  firewall_ids = [hcloud_firewall.common-firewall.id]
+
+  public_net {
+    ipv6_enabled = true
+  }
+
+  network {
+    network_id = hcloud_network.network.id
+  }
+
+  depends_on = [
+    hcloud_network_subnet.network-subnet
+  ]
+}
