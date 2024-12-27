c39b2bcd43a3b8cffde4c4aa1d682fbbb5d25b34
Yunior Alayo <yunior.alayo@gmail.com>
2024-12-27 01:54:43 +0100

    Added the user data directly on code.

--- projects/main/terraform/main.tf
+++ projects/main/terraform/main.tf
@@ -85,9 +85,25 @@ resource "hcloud_server" "immo" {
   location    = "nbg1"
   server_type = "cax11" 
   keep_disk   = true
-  ssh_keys    = [data.hcloud_ssh_key.immo_ssh_key.id]
-  user_data   = "${base64encode(file("./immo_bootscript.sh"))}" 
+  ssh_keys    = [data.hcloud_ssh_key.immo_ssh_key.id] 
   firewall_ids = [hcloud_firewall.common-firewall.id]
+
+  user_data = <<-EOF
+    #!/bin/bash
+    sudo apt-get update
+    sudo apt-get -y upgrade
+    sudo apt-get -y install apt-transport-https ca-certificates curl software-properties-common
+    curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add
+    sudo add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu  $(lsb_release -cs)  stable"
+    sudo apt-get update
+    sudo apt-get -y install docker-ce
+    sudo curl -L "https://github.com/docker/compose/releases/download/1.24.0/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
+    sudo chmod +x /usr/local/bin/docker-compose
+    sudo usermod -aG docker ubuntu
+    sudo chmod 777 /var/run/docker.sock
+    sudo sed -i 's/^#*\s*PermitRootLogin\s.*/PermitRootLogin prohibit-password/' /etc/ssh/sshd_config
+    sudo systemctl restart ssh
+  EOF
 
   public_net {
     ipv6_enabled = true
