9b6b398cb71fe8e550deb73232c8648065022aa2
Yunior Alayo <yunior.alayo@gmail.com>
2024-12-27 10:31:34 +0100

    Updated the user_data script to install docker.

--- projects/main/terraform/main.tf
+++ projects/main/terraform/main.tf
@@ -91,16 +91,16 @@ resource "hcloud_server" "immo" {
   user_data = <<-EOF
     #!/bin/bash
     sudo apt-get update
-    sudo apt-get -y upgrade
-    sudo apt-get -y install apt-transport-https ca-certificates curl software-properties-common
-    curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add
-    sudo add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu  $(lsb_release -cs)  stable"
+    sudo apt-get install ca-certificates curl
+    sudo install -m 0755 -d /etc/apt/keyrings
+    sudo curl -fsSL https://download.docker.com/linux/ubuntu/gpg -o /etc/apt/keyrings/docker.asc
+    sudo chmod a+r /etc/apt/keyrings/docker.asc
+    echo \
+        "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.asc] https://download.docker.com/linux/ubuntu \
+        $(. /etc/os-release && echo "$VERSION_CODENAME") stable" | \
+    sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
     sudo apt-get update
-    sudo apt-get -y install docker-ce
-    sudo curl -L "https://github.com/docker/compose/releases/download/1.24.0/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
-    sudo chmod +x /usr/local/bin/docker-compose
-    sudo usermod -aG docker ubuntu
-    sudo chmod 777 /var/run/docker.sock
+    sudo apt-get install docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin
     sudo sed -i 's/^#*\s*PermitRootLogin\s.*/PermitRootLogin prohibit-password/' /etc/ssh/sshd_config
     sudo systemctl restart ssh
   EOF
