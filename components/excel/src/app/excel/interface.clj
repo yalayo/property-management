d892480bdad097ca8978391946ca5164ee7b9027
Yunior Alayo <yunior.alayo@gmail.com>
2024-12-27 13:11:57 +0100

    Removed not needed function.

--- components/excel/src/app/excel/interface.clj
+++ components/excel/src/app/excel/interface.clj
@@ -1,8 +1,5 @@
 (ns app.excel.interface
   (:require [app.excel.core :as core]))
 
-(defn process-details [data]
-  (core/process-details data))
-
 (defn process [input-stream]
   (core/process input-stream))
