a825d1d5355a95d4cc4d570eeda2ad1f246f7b46
unknown <denmanuel888@gmail.com>
2025-01-11 19:20:45 -0500

    Changed text color from white to black

--- components/html/src/app/html/upload_details.clj
+++ components/html/src/app/html/upload_details.clj
@@ -83,7 +83,7 @@
      [:div
       {:class "max-w-xl lg:max-w-lg"}
       [:h2
-       {:class "text-4xl font-semibold tracking-tight text-white"}
+       {:class "text-4xl font-semibold tracking-tight text-black"}
        "Subscribe to our newsletter"]
       [:p
        {:class "mt-4 text-lg text-gray-300"}
@@ -99,12 +99,12 @@
           :autocomplete "email",
           :required "",
           :class
-          "min-w-0 flex-auto rounded-md bg-white/5 px-3.5 py-2 text-base text-white outline outline-1 -outline-offset-1 outline-white/10 placeholder:text-gray-500 focus:outline focus:outline-2 focus:-outline-offset-2 focus:outline-indigo-500 sm:text-sm/6",
+          "min-w-0 flex-auto rounded-md bg-black/5 px-3.5 py-2 text-base text-black outline outline-1 -outline-offset-1 outline-white/10 placeholder:text-gray-500 focus:outline focus:outline-2 focus:-outline-offset-2 focus:outline-indigo-500 sm:text-sm/6",
           :placeholder "Enter your email"}]
         [:button
          {:type "submit",
           :class
-          "flex-none rounded-md bg-indigo-500 px-3.5 py-2.5 text-sm font-semibold text-white shadow-sm hover:bg-indigo-400 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-500"}
+          "flex-none rounded-md bg-indigo-500 px-3.5 py-2.5 text-sm font-semibold text-black shadow-sm hover:bg-indigo-400 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-500"}
          "Subscribe"]]]]]]])
 
 (defn page []
