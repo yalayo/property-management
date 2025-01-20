9a808cb20bd1a23a394ffbf4be2fc6a3ad06e4dc
Yunior Alayo <yunior.alayo@gmail.com>
2025-01-20 12:41:36 +0100

    Added logs to the main project.

--- projects/main/src/app/main/core.clj
+++ projects/main/src/app/main/core.clj
@@ -1,15 +1,26 @@
 (ns app.main.core
   (:require [com.stuartsierra.component :as component]
+            [com.brunobonacci.mulog :as mu]
             [app.storage.interface :as storage]
             [app.route.interface :as route]
             [app.server.core :as server]))
 
 (def config {:db-spec {:dbtype "postgres"
-                       :host (if (= (System/getenv "ENVIRONMENT") "prod") "prod-db" "localhost")
+                       :host (if (= (System/getenv "ENVIRONMENT") "prod") (System/getenv "DB_HOST") "localhost")
                        :dbname "property-management"
                        :username "user"
-                       :password (if (= (System/getenv "ENVIRONMENT") "prod") "hrdata@2024" "volley@2024")
+                       :password (if (= (System/getenv "ENVIRONMENT") "prod") (System/getenv "DB_PASSWORD") "volley@2024")
                        :dataSourceProperties {:socketTimeout 30}}})
+
+(defn init-logging []
+  (mu/start-publisher!
+   {:type :multi
+    :publishers
+    [{:type :console}
+     {:type :custom
+      :fqn-function "app.logs.interface/telegram-publisher"
+      :bot-token (System/getenv "BOT_TOKEN")
+      :chat-id (System/getenv "CHAT_ID")}]}))
 
 (defn create-system [config]
   (component/system-map
@@ -20,10 +31,11 @@
             [:datasource :route])))
 
 (defn -main []
+  (init-logging)
   (let [system (-> config
                    (create-system)
                    (component/start-system))]
-    (println "Starting system for project: main with config")
+    (mu/log :system-started :message "Starting system!")
     (.addShutdownHook
      (Runtime/getRuntime)
      (new Thread #(component/stop-system system)))))
