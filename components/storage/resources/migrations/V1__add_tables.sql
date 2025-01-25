aa0f31bf9aa41f3783df61ad3b220fa5e5d95b7d
Kirkmasterix <enmanuel@gmail.com>
2025-01-25 09:30:47 -0500

    Added columns to the accounts table.

--- components/storage/resources/migrations/V1__add_tables.sql
+++ components/storage/resources/migrations/V1__add_tables.sql
@@ -1,16 +1,18 @@
 -- Table to store user accounts
 create table accounts
 (
-    email text primary key,
+    user_id text primary key,
+    email text unique,
     password text not null,
+    veryfied boolean default false,
     created_at timestamp not null default current_timestamp
 );
 
 -- Table required for jdbc-session-store
-CREATE TABLE session_store
+create table session_store
 (
-    session_id VARCHAR(36) NOT NULL PRIMARY KEY,
-    idle_timeout BIGINT,
-    absolute_timeout BIGINT,
-    value BYTEA
+    session_id varchar(36) not null primary key,
+    idle_timeout bigint,
+    absolute_timeout bigint,
+    value bytea
 );
