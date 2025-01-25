e327245b3846cf848ce2f257c2b0668ebdecd99f
Yunior Alayo <yunior.alayo@gmail.com>
2025-01-25 14:00:20 +0100

    Added handler to show a list of flags

--- components/flags/src/app/flags/core.clj
+++ components/flags/src/app/flags/core.clj
@@ -1,1 +1,13 @@
-(ns app.flags.core)
+(ns app.flags.core
+  (:require [app.html.interface :as html]
+            [app.flags.list :as flags]))
+
+(def flags-handler
+  {:name ::get
+   :enter (fn [context]
+            (assoc context :response (html/respond-with-params flags/content [] "Feature flags")))})
+
+(def routes
+  #{["/flags"
+     :get flags-handler
+     :route-name ::flags]})
