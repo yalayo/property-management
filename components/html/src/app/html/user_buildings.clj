48a10b751cfb38a0a6318b8d7d176a4d59eff22e
lgcanetti <lgcanetti@gmail.com>
2025-01-29 18:01:17 -0500

    Add default picture for buildings

--- components/html/src/app/html/user_buildings.clj
+++ components/html/src/app/html/user_buildings.clj
@@ -1,16 +1,16 @@
 (ns app.html.user-buildings)
 
 (def data  {:cur-user "Current user"
-            :buildings [{:id 1 :name "Building 1" :address "Address 1" :pic "https://unsplash.com/es/fotos/una-gran-casa-blanca-sentada-al-costado-de-una-carretera-27dyESJ0RIk"}
-                        {:id 2 :name "Building 2" :address "Address 2" :pic "https://unsplash.com/es/fotos/un-patio-con-fogata-y-zona-de-estar-E_6gn_IdJMs"}
-                        {:id 3 :name "Building 3" :address "Address 3" :pic "https://unsplash.com/es/fotos/una-vista-aerea-de-una-casa-en-el-otono-ljnOUFtwzFA"}
-                        {:id 4 :name "Building 4" :address "Address 4" :pic "https://unsplash.com/es/fotos/una-piscina-con-tumbonas-a-su-alrededor-rCHO0tFxLlk"}
-                        {:id 5 :name "Building 5" :address "Address 5" :pic "https://unsplash.com/es/fotos/una-vista-aerea-de-una-casa-en-el-otono-pWgdeJMKDEM"}
-                        {:id 6 :name "Building 6" :address "Address 6" :pic "https://unsplash.com/es/fotos/vista-aerea-de-una-casa-con-piscina-USpH76SXqS8"}
-                        {:id 7 :name "Building 7" :address "Address 7" :pic "https://unsplash.com/es/fotos/una-casa-en-el-bosque-con-un-camino-que-conduce-a-ella-ysM9uXVGpyw"}
-                        {:id 8 :name "Building 8" :address "Address 8" :pic "https://unsplash.com/es/fotos/una-sauna-de-madera-en-medio-de-una-zona-boscosa-30Rta33lHrk"}
-                        {:id 9 :name "Building 9" :address "Address 9" :pic "https://unsplash.com/es/fotos/una-pequena-cabana-en-el-bosque-con-una-cubierta-5Bnb9Ec07HI"}
-                        {:id 10 :name "Building 10" :address "Address 10" :pic "https://unsplash.com/es/fotos/una-gran-casa-blanca-con-una-puerta-roja-yIZZucnwJWA"}]})
+            :buildings [{:id 1 :name "Building 1" :address "Address 1" :pic "def-building.jpg"}
+                        {:id 2 :name "Building 2" :address "Address 2" :pic "def-building.jpg"}
+                        {:id 3 :name "Building 3" :address "Address 3" :pic "def-building.jpg"}
+                        {:id 4 :name "Building 4" :address "Address 4" :pic "def-building.jpg"}
+                        {:id 5 :name "Building 5" :address "Address 5" :pic "def-building.jpg"}
+                        {:id 6 :name "Building 6" :address "Address 6" :pic "def-building.jpg"}
+                        {:id 7 :name "Building 7" :address "Address 7" :pic "def-building.jpg"}
+                        {:id 8 :name "Building 8" :address "Address 8" :pic "def-building.jpg"}
+                        {:id 9 :name "Building 9" :address "Address 9" :pic "def-building.jpg"}
+                        {:id 10 :name "Building 10" :address "Address 10" :pic "def-building.jpg"}]})
 
 (defn get-buildings []
   [:div.container.mx-auto
