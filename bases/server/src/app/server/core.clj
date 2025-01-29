93bfaf94001b8813549c0c76edc3aef30736e515
Yunior Alayo <yunior.alayo@gmail.com>
2025-01-29 15:49:26 +0100

    Updated the logging.

--- bases/server/src/app/server/core.clj
+++ bases/server/src/app/server/core.clj
@@ -1,5 +1,6 @@
 (ns app.server.core
 	(:require [com.stuartsierra.component :as component]
+            [com.brunobonacci.mulog :as mu]
             [io.pedestal.http :as http]
             [io.pedestal.http.route :as route]
             [io.pedestal.http.ring-middlewares :as middlewares]
@@ -9,7 +10,6 @@
   component/Lifecycle
   
   (start [component]
-         (println "Starting server-component")
          (let [session-interceptor (middlewares/session {:store (jdbc-ring-session/jdbc-store (datasource) {:table :session_store})})
                server (-> {:env :prod
                            ::http/routes (route/expand-routes (get-in routes [:routes (:active-route config)]))
@@ -21,12 +21,13 @@
                           (update ::http/interceptors concat [session-interceptor])
                           (http/create-server)
                           (http/start))]
+           (mu/log :server-started :message (str "Starting server with " (name (:active-route config)) " routes!"))
            (assoc component :server server)))
   
   (stop [component]
-        (println "Stopping server-component")
         (when-let [server (:server component)]
           (http/stop server))
+        (mu/log :server-stopped :message "Stopping web server!")
         (assoc component :server nil)))
 
 (defn server-component
