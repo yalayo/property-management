904b1c30de783a82a74fae5fa5da6407fa626ade
lgcanetti <lgcanetti@gmail.com>
2025-01-30 19:11:45 -0500

    Prepare dhashboard to use htmx to toggle dropdown menus

--- components/html/src/app/html/dashboard.clj
+++ components/html/src/app/html/dashboard.clj
@@ -29,7 +29,7 @@
                     {:name "Reports" :href "#"}]} 
     :profile {:menu [{:name "Your Profile" :href "#"} 
                      {:name "Settings" :href "#"} 
-                     {:name "Sign out" :href "#"}]}})
+                     {:name "Sign out" :href "/sign-in"}]}})
 
 (defn- load-profile-menu
   "Renders the user profile menus.\n
@@ -126,9 +126,11 @@
          [:div
           [:button
            {:type "button",
+            :id "user-menu-button",
+            :hx-target "#dropdown-menu",;;htmx code to control landscape dropdown menu
+            :hx-swap "toggle",
             :class
             "relative flex max-w-xs items-center rounded-full bg-gray-800 text-sm focus:outline-none focus:ring-2 focus:ring-white focus:ring-offset-2 focus:ring-offset-gray-800",
-            :id "user-menu-button",
             :aria-expanded "false",
             :aria-haspopup "true"}
            [:span {:class "absolute -inset-1.5"}]
@@ -139,8 +141,9 @@
              :alt ""}]]]
          (comment
            "Dropdown menu, show/hide based on menu state.\n\n                Entering: \"transition ease-out duration-100\"\n                  From: \"transform opacity-0 scale-95\"\n                  To: \"transform opacity-100 scale-100\"\n                Leaving: \"transition ease-in duration-75\"\n                  From: \"transform opacity-100 scale-100\"\n                  To: \"transform opacity-0 scale-95\"")
-         [:div
-          {:class
+         [:div#dropdown-menu
+          {:style "display: none;",
+           :class
            "absolute right-0 z-10 mt-2 w-48 origin-top-right rounded-md bg-white py-1 shadow-lg ring-1 ring-black ring-opacity-5 focus:outline-none",
            :role "menu",
            :aria-orientation "vertical",
@@ -153,6 +156,8 @@
        (comment "Mobile menu button")
        [:button
         {:type "button",
+         :hx-target "#mobile-menu",;;htmx code to control mobile menu
+         :hx-swap "toggle",
          :class
          "relative inline-flex items-center justify-center rounded-md bg-gray-800 p-2 text-gray-400 hover:bg-gray-700 hover:text-white focus:outline-none focus:ring-2 focus:ring-white focus:ring-offset-2 focus:ring-offset-gray-800",
          :aria-controls "mobile-menu",
@@ -184,8 +189,9 @@
            :stroke-linejoin "round",
            :d "M6 18L18 6M6 6l12 12"}]]]]]]
     (comment "Mobile menu, show/hide based on menu state.")
-    [:div
-     {:class "md:hidden", :id "mobile-menu"}
+    [:div#mobile-menu;;Menu mobile (Portrait)
+     {:style "display: none;",
+      :class "md:hidden", :id "mobile-menu"}
      [:div
       {:class "space-y-1 px-2 pb-3 pt-2 sm:px-3"}
       (comment
@@ -228,7 +234,7 @@
            :d
            "M14.857 17.082a23.848 23.848 0 005.454-1.31A8.967 8.967 0 0118 9.75v-.7V9A6 6 0 006 9v.75a8.967 8.967 0 01-2.312 6.022c1.733.64 3.56 1.085 5.455 1.31m5.714 0a24.255 24.255 0 01-5.714 0m5.714 0a3 3 0 11-5.714 0"}]]]]
       [:div
-       {:class "mt-3 space-y-1 px-2"}; :style "display: none;"
+       {:class "mt-3 space-y-1 px-2"}
        (load-profile-menu true)]]]]
    [:header
     {:class "bg-white shadow"}
