e2091b80cff4211a49cc72e974a3200bbd82bcf8
lgcanetti <lgcanetti@gmail.com>
2025-02-01 17:35:51 -0500

    Isolate apartment-details

---
+++ components/html/src/app/html/apartment_details.clj
@@ -0,0 +1,42 @@
+(ns app.html.apartment-details)
+
+(def ^{:private true} apartments [{:id 1 :name "Apartamento Central"}
+                 {:id 2 :name "Villa Vista"}
+                 {:id 3 :name "Residencia Aurora"}])
+
+(defn- construct-form-details [id name]
+  [:div
+   [:h2
+    {:class "text-3xl font-bold tracking-tight text-gray-900"}
+    (str "ID: " id)]
+   [:p
+    {:class "text-3xl font-bold tracking-tight text-gray-900"}
+    (str "Name: " name)]])
+
+(defn show-apartment-details [details]
+  (let [[id name] details]
+    (println "ID in method: " id)
+    (println "Name in method: " name)
+    (construct-form-details id name)))
+
+
+(defn get-apartments-detail []
+  [:div.container.mx-auto
+  [:ul
+   {:class "space-y-4"}
+   (for [{:keys [id name]} apartments]
+     [:li
+      {:key id, :class "border rounded-md p-4 shadow-sm"}
+      [:form {:action "/dashboard" :method "post"}
+       [:h2
+        {:class "text-3xl font-bold tracking-tight text-gray-900"}
+        (str "ID: " id)]
+       [:p
+        {:class "text-3xl font-bold tracking-tight text-gray-900"}
+        (str "Name: " name)]
+       [:input {:type "hidden", :name "id",  :value id}]
+       [:input {:type "hidden", :name "name", :value name}]
+       [:button
+        {:type "submit",
+         :class "flex-none rounded-md bg-indigo-500 px-3.5 py-2.5 text-sm font-semibold text-black shadow-sm hover:bg-indigo-400 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-500"}
+        "Show Details"]]])]])
--- components/html/src/app/html/dashboard.clj
+++ components/html/src/app/html/dashboard.clj
@@ -1,25 +1,5 @@
 (ns app.html.dashboard)
 
-(def apartments [{:id 1 :name "Apartamento Central"}
-                 {:id 2 :name "Villa Vista"}
-                 {:id 3 :name "Residencia Aurora"}])
-
-(defn construct-form-details [id name]
-  [:div
-   [:h2
-    {:class "text-3xl font-bold tracking-tight text-gray-900"}
-    (str "ID: " id)]
-   [:p
-    {:class "text-3xl font-bold tracking-tight text-gray-900"}
-    (str "Name: " name)]])
-
-(defn show-apartment-details [details]
-  (let [[id name] details] 
-    (println "ID in method: " id)
-    (println "Name in method: " name)
-    (construct-form-details id name)
-    ))
-
 (def ^{:private true} menu-config
   "Configuration for the navigation bar and user profile menus."
    {:navbar {:menu [{:name "Dashboard" :href "#"} 
@@ -247,22 +227,4 @@
 [:main
  [:div
   {:class "mx-auto max-w-7xl px-4 py-6 sm:px-6 lg:px-8"}
-     (:content content)
-  [:ul
-   {:class "space-y-4"}
-   (for [{:keys [id name]} apartments]
-     [:li
-      {:key id, :class "border rounded-md p-4 shadow-sm"}
-      [:form {:action "/dashboard" :method "post"}
-      [:h2
-       {:class "text-3xl font-bold tracking-tight text-gray-900"}
-       (str "ID: " id)]
-      [:p
-       {:class "text-3xl font-bold tracking-tight text-gray-900"}
-       (str "Name: " name)]
-       [:input {:type "hidden", :name "id",  :value id}]
-       [:input {:type "hidden", :name "name", :value name}]
-       [:button
-        {:type "submit",
-         :class "flex-none rounded-md bg-indigo-500 px-3.5 py-2.5 text-sm font-semibold text-black shadow-sm hover:bg-indigo-400 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-500"}
-        "Show Details"]]])]]]])
+     (:content content)]]])
