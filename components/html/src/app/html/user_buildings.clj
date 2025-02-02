bfa3e39d7b6212b236d5a9efb58ed1fdb71f3ad7
lgcanetti <lgcanetti@gmail.com>
2025-02-02 18:47:08 -0500

    Use Post method tos send params to building-apartments

--- components/html/src/app/html/user_buildings.clj
+++ components/html/src/app/html/user_buildings.clj
@@ -5,17 +5,15 @@
                         {:id "0002" :name "Building 2" :address "Address 2" :pic "def-building.jpg"}
                         {:id "0003" :name "Building 3" :address "Address 3" :pic "def-building.jpg"}]})
 
-(defn- get-detail-url [id name]
-  (str "/building-apartments?id=" id "&name=" name))
-
 (defn get-buildings []
   [:div.container.mx-auto
    [:h1 {:class "text-2xl font-semibold text-gray-900"} (str (:cur-user data) " s'Buildings:")]
    [:ul
     {:role "list", :class "divide-y divide-gray-100"}
     (for [building (:buildings data)]
-      [:li
-       {:class "flex justify-between gap-x-6 py-5"}
+      [:li 
+       {:key (:id building),
+        :class "flex justify-between gap-x-6 py-5"}
        [:div
         {:class "flex min-w-0 gap-x-4"}
         [:img
@@ -24,9 +22,14 @@
           :alt "Picture of building"}]
         [:div
          {:class "min-w-0 flex-auto"}
-         [:p
-          {:class "text-sm/6 font-semibold text-gray-900"}
-          [:a {:href (get-detail-url (:id building) (:name building)) :class "hover:underline"} (:name building)]]
+         [:form {:action "/building-apartments" :method "post"}
+          [:input {:type "hidden", :name "id",  :value (:id building)}]
+          [:input {:type "hidden", :name "name", :value (:name building)}]
+          [:button
+           {:type "submit",
+            :class
+            "border-none bg-transparent p-2 rounded text-blue-500 hover:text-blue-700 cursor-pointer transition duration-300"}
+            (:name building)]]
          [:p
           {:class "mt-1 truncate text-xs/5 text-gray-500"}
           (:address building)]]]
@@ -36,4 +39,5 @@
          "Other data here!"]
         [:p
          {:class "mt-1 text-xs/5 text-gray-500"}
-         "Other data here!"]]])]])
+         "Other data here!"]
+        ]])]])
