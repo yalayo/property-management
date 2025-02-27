a34e780afa5f5d9ef4c8587f3bbe3700ce2165c4
Yunior Alayo <yunior.alayo@gmail.com>
2025-02-27 06:45:26 +0100

    Created a new feature form and added to the feature list.

--- components/property/src/app/property/list.clj
+++ components/property/src/app/property/list.clj
@@ -1,4 +1,5 @@
-(ns app.property.list)
+(ns app.property.list
+  (:require [app.property.new-property :as new-property]))
 
 (defn any-missing-value? [m]
   (some nil? (vals m)))
@@ -60,6 +61,9 @@
 
 (defn content [properties]
   [:main
+   (new-property/get-new-property-form)
+   [:button {:class "inline-block shrink-0 rounded-md border border-blue-600 bg-blue-600 px-12 py-3 text-sm font-medium text-white transition hover:bg-transparent hover:text-blue-600 focus:outline-none focus:ring active:text-blue-500 dark:hover:bg-blue-700 dark:hover:text-white"
+             :_ "on click remove .translate-x-full from #slide-over-new-property then remove .opacity-0 from #new-property"} "Hinzufügen"]
    [:div {:id "table" :class "mx-auto max-w-7xl px-4 py-6 sm:px-6 lg:px-8 divide-y divide-gray-100"}
     (map property-info properties)]])
 
---
+++ components/property/src/app/property/new_property.clj
@@ -0,0 +1,99 @@
+(ns app.property.new-property)
+
+(defn get-new-property-form []
+  [:form#add-property
+   {:class "relative z-10 pointer-events-none",
+    :aria-labelledby "slide-over-title",
+    :role "dialog",
+    :aria-modal "true"
+    :hx-post "/new-property"}
+   (comment
+     "Background backdrop, show/hide based on slide-over state.
+      Entering: \"ease-in-out duration-500\"\n      From: \"opacity-0\"\n      To: \"opacity-100\"\n    Leaving: \"ease-in-out duration-500\"\n      From: \"opacity-100\"\n      To: \"opacity-0\"")
+   [:div#new-property
+    {:class "fixed inset-0 bg-gray-500/75 transition-opacity ease-in-out duration-500 opacity-0",
+     :aria-hidden "true"}]
+   [:div
+    {:class "fixed inset-0 overflow-hidden"}
+    [:div
+     {:class "absolute inset-0 overflow-hidden"}
+     [:div
+      {:class
+       "pointer-events-none fixed inset-y-0 right-0 flex max-w-full pl-10"}
+      (comment
+        "Slide-over panel, show/hide based on slide-over state.\n\n  Entering: \"transform transition ease-in-out duration-500 sm:duration-700\"\n            From: \"translate-x-full\"\n            To: \"translate-x-0\"\n          Leaving: \"transform transition ease-in-out duration-500 sm:duration-700\"\n            From: \"translate-x-0\"\n            To: \"translate-x-full\"")
+      [:div#slide-over-new-property
+       {:class "pointer-events-auto w-screen max-w-md transform transition ease-in-out duration-500 sm:duration-700 translate-x-full"}
+       [:div
+        {:class
+         "flex h-full flex-col overflow-y-scroll bg-white shadow-xl"} 
+        [:div
+         {:class "flex-1 overflow-y-auto px-4 py-6 sm:px-6"}
+         [:div
+          {:class "flex items-start justify-between"}
+          [:h2
+           {:class "text-lg font-medium text-gray-900",
+            :id "slide-over-title"}
+           "Here you can add a new feature!"]
+          [:div
+           {:class "ml-3 flex h-7 items-center"}
+           [:button#close-panel
+            {:type "button",
+             :class
+             "relative -m-2 p-2 text-gray-400 hover:text-gray-500",
+             :_"on click add .translate-x-full to #slide-over-new-property then add .opacity-0 to #new-property"}
+            [:span {:class "absolute -inset-0.5"}]
+            [:span {:class "sr-only"} "Close panel"]
+            [:svg
+             {:class "w-6 h-6",
+              :fill "none",
+              :viewBox "0 0 24 24",
+              :stroke-width "1.5",
+              :stroke "currentColor",
+              :aria-hidden "true",
+              :data-slot "icon"}
+             [:path
+              {:stroke-linecap "round",
+               :stroke-linejoin "round",
+               :d "M6 18 18 6M6 6l12 12"}]]]]]
+         [:div
+          {:class "mt-8"}
+          [:form#form-new-ft {:hx-post "/new-feature",  ;; Endpoint to which the form will be sent
+                              :hx-target "#response",   ;; Element where the response will be displayed
+                              :hx-swap "innerHTML",     ;; How the answer will be inserted
+                              :hx-trigger "submit",     ;; Trigger request on form submission
+                              :_ "on submit trigger click on #close-panel"} ;; Close the panel after submitting the form    
+          [:div
+           {:class "flow-root"}
+           (comment "Formulary content") 
+            [:div
+             {:class "mt-10 grid grid-cols-1 gap-x-6 gap-y-8 sm:grid-cols-6"}
+             [:div
+              {:class "sm:col-span-6"}
+              [:label
+               {:for "ft-name", :class "block text-sm/6 font-medium text-gray-900"}
+               "Name"]
+              [:div {:class "mt-2"}
+               [:div
+                {:class
+                 "flex items-center rounded-md bg-white pl-3 border border-gray-300 focus-within:border-2 focus-within:border-indigo-600"}
+                [:div
+                 {:class "shrink-0 text-base text-gray-500 select-none sm:text-sm/6"}
+                 "Feature:"]
+                [:input
+                 {:type "text",
+                  :name "ft-name",
+                  :id "ft-name",
+                  :class
+                  "w-full py-2 text-base text-gray-900 placeholder:text-gray-400 focus:outline-none sm:text-sm/6",
+                  :placeholder "new feature"}]]]]]]]]]
+        [:div {:class "border-t border-gray-200 px-4 py-6 sm:px-6"}
+         [:div
+          {:class "mt-6"}
+          [:a
+           {:href "#",
+            :type "submit",
+            :class
+            "flex items-center justify-center rounded-md border border-transparent bg-indigo-600 px-6 py-3 text-base font-medium text-white shadow-xs hover:bg-indigo-700",
+            :_"on click trigger click on #close-panel then trigger submit on #form-new-ft"}
+           "Add new feature"]]]]]]]]])
