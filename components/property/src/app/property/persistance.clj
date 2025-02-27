7980078b171e6e9e4f336417f4dfe10f9b7fedd1
Yunior Alayo <yunior.alayo@gmail.com>
2025-02-27 07:42:07 +0100

    Updated to use with properties.

--- components/property/src/app/property/persistance.clj
+++ components/property/src/app/property/persistance.clj
@@ -8,23 +8,16 @@
              {:db/ident :name
               :db/valueType :db.type/string
               :db/unique :db.unique/identity
-              :db/cardinality :db.cardinality/one}
-             {:db/ident :enabled
-              :db/valueType :db.type/boolean
-              :db/unique :db.unique/identity
               :db/cardinality :db.cardinality/one}])
 
-(defn create-feature-flag [name]
-  (storage/transact [{:id (java.util.UUID/randomUUID) :name name :enabled false}] "feature-flags"))
+(defn create-property [name]
+  (storage/transact [{:id (java.util.UUID/randomUUID) :name name :enabled false}] "properties"))
 
-(defn enable-feature-flag [id]
-  (storage/transact [{:id id :enabled true}] "feature-flags"))
-
-(defn list-feature-flags []
+(defn list-properties []
   (storage/query
-   "[:find ?id ?name ?enabled :where [?e :id ?id] [?e :name ?name] [?e :enabled ?enabled]]" ""))
+   "[:find ?id ?name :where [?e :id ?id] [?e :name ?name]]" "properties"))
 
 (comment
   "Store the schema, for the moment let's do it manually"
-  (storage/transact schema "flags")
+  (storage/transact schema "properties")
   )
