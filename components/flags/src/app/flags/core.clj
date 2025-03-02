8b346996cbce90dd77ea7daa25edf9ec8fa29d3c
Yunior Alayo <yunior.alayo@gmail.com>
2025-03-02 18:34:38 +0100

    Updated to add new flag.

--- components/flags/src/app/flags/core.clj
+++ components/flags/src/app/flags/core.clj
@@ -1,7 +1,11 @@
 (ns app.flags.core
-  (:require [app.html.interface :as html]
+  (:require [io.pedestal.http.body-params :as body-params]
+            [io.pedestal.http.params :as params]
+            [app.html.interface :as html]
             [app.html.layout :as layout]
-            [app.flags.list :as flags]))
+            [app.flags.list :as flags]
+            [app.flags.new-flag :as new-flag]
+            [app.property.persistance :as persistance]))
 
 (def flags-handler
   {:name ::get
@@ -12,14 +16,15 @@
 (def new-flag-handler
   {:name ::post
    :enter (fn [context]
+            (println "Params: " (-> context :request :params))
             (let [params (-> context :request :params)
                   flag-name (:name params)]
-              (assoc context :response (flags/flag-info {:name flag-name}))))})
+              (assoc context :response (html/respond new-flag/get-new-flag-form "New flag"))))})
 
 (def routes
   #{["/flags"
      :get flags-handler
      :route-name ::flags]
     ["/new-flag"
-     :post new-flag-handler
+     :post [(body-params/body-params) params/keyword-params new-flag-handler]
      :route-name ::new-flag]})
