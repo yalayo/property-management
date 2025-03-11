77f224ba4461dad59187de10430bc5ffba84b795
Yunior Alayo <yunior.alayo@gmail.com>
2025-03-11 15:57:50 +0100

    Parsing the dates and fixing multiline issue.

--- development/src/finance.clj
+++ development/src/finance.clj
@@ -175,18 +175,31 @@
          current []
          grouped []]
     (if (empty? remaining)
-      (conj grouped (str/join " " current))  ; Add last transaction
+      (if (empty? current) grouped (conj grouped (str/join " " current)))  ; Ensure last transaction is added
       (let [line (first remaining)
             rest-lines (rest remaining)]
         (if (starts-with-date? line)
           (recur rest-lines [line] (if (empty? current) grouped (conj grouped (str/join " " current))))
           (recur rest-lines (conj current line) grouped))))))
 
+;; Processing dates
+(def german-months
+  {"Jan" "01", "Feb" "02", "März" "03", "Apr" "04", "Mai" "05", "Juni" "06"
+   "Juli" "07", "Aug" "08", "Sep" "09", "Okt" "10", "Nov" "11", "Dez" "12"})
+
+(defn parse-german-date [date-str]
+  (let [[_ day month year] (re-matches #"(\d{1,2})\. (\w+)\. (\d{4})" date-str)
+        month-num (get german-months month)]
+    (if month-num
+      (LocalDate/parse (str year "-" month-num "-" (format "%02d" (Integer/parseInt day)))
+                       (DateTimeFormatter/ofPattern "yyyy-MM-dd"))
+      (throw (Exception. (str "Invalid month: " month))))))
+
 (defn parse-line-2 [line]
-  (let [[_ date text amount]
-        (re-matches #"(\d{1,2}\. \w{3}\. \d{4}) (.+?) ([\d\.,]+)" (clean-str line))]
+  (let [[_ date text amount _]
+        (re-matches #"(\d{1,2}\. [A-ZÄÖÜa-zäöüß]+\. \d{4}) (.*?) (\d{1,3}(\.\d{3})*,\d{2})(.*)" (clean-str line))]
     (when (and date text amount)
-      {:date date
+      {:date (parse-german-date date)
        :text text
        :amount (parse-amount-apotheke amount)})))
 
@@ -195,7 +208,6 @@
        (map clean-str)
        (remove str/blank?)
        (merge-multi-line-transactions)
-       (println)
        (map parse-line-2)
        (remove nil?)))
 
