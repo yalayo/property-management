3d297ab3f818572b9d909b0d00f1773686724795
lgcanetti <lgcanetti@gmail.com>
2025-03-12 10:26:30 -0400

    Bug fixed

--- components/html/src/app/html/layout.clj
+++ components/html/src/app/html/layout.clj
@@ -239,5 +239,4 @@
 [:main
  [:div
   {:class "mx-auto max-w-7xl px-4 py-6 sm:px-6 lg:px-8"}
-     [:div#dashboard-main-content 
-      (:content content)}]]])
+     [:div#dashboard-dyn-ctn (:content content)]]]])
