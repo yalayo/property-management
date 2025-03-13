3aa9fb8fc62c6b2aa9398548689ff708df6d092d
Kirkmasterix <enmanuel@gmail.com>
2025-03-13 19:37:37 -0400

    Added link to upload property bank file

--- components/property/src/app/property/core.clj
+++ components/property/src/app/property/core.clj
@@ -32,15 +32,16 @@
     :enter (fn [context]
              (let [session (-> context :request :session)
                    name (-> context :request :path-params :name)]
-               (println "Recarga la pagina del handler que envia el form")
-               (assoc context :response (html/respond-with-params properties/get-property-details name"Property"))))})
+               (println "Show the form to upload property")
+               (assoc context :response (html/respond-with-params properties/get-property-details name "Property"))))})
 
-(def post-upload-clients-handler
+(def post-upload-property-handler
   {:name ::post
    :enter (fn [context]
             (let [multipart-data (:multipart-params (-> context :request))
                   file (get multipart-data "file")
                   file-input-stream (:tempfile file)]
+              (println "Enter to the handler to proccess the propertie's bank data")
               (if (some? file-input-stream)
                 (let [result (flatten (excel/property-bank-data file-input-stream))]
                     (assoc context :response {:status 200
@@ -56,12 +57,12 @@
   #{["/properties"
      :get properties-handler
      :route-name ::properties]
-    ["/propertie/:name"
+    ["/property/:name"
      :get [(body-params/body-params) params/keyword-params propertie-handler]
      :route-name ::propertie]
     ["/upload-property-details"
-     :post [(ring-mw/multipart-params) post-upload-clients-handler]
-     :route-name ::upload-clients-details]
+     :post [(ring-mw/multipart-params) post-upload-property-handler]
+     :route-name ::upload-property-details]
     ["/new-property"
      :post [(body-params/body-params) params/keyword-params new-property-handler]
      :route-name ::new-property]})
--- components/property/src/app/property/list.clj
+++ components/property/src/app/property/list.clj
@@ -43,10 +43,9 @@
         [:div {:class "mt-3 text-center sm:ml-4 sm:mt-0 sm:text-left"}
          [:div {:class "mt-2"}
           [:p {:class "text-sm text-gray-500"} "Fehlende Informationen. Bitte überprüfen Sie Ihre Excel-Datei."]]]]]
-      [:button
-       {:type "button",
-        :class "inline-flex items-center rounded-md bg-indigo-600 px-3 py-2 text-sm font-semibold text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
-        :onclick (str "window.open('/propertie/" (:name property) "', '_blank');")} 
+      [:a
+       {:class "inline-flex items-center rounded-md bg-indigo-600 px-3 py-2 text-sm font-semibold text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
+        :href (str "/property/" (:name property))} 
        [:svg
         {:class "-ml-0.5 mr-1.5 h-5 w-5",
          :viewBox "0 0 20 20",
@@ -67,37 +66,43 @@
    [:div {:id "table" :class "mx-auto max-w-7xl px-4 py-6 sm:px-6 lg:px-8 divide-y divide-gray-100"}
     (map property-info properties)]])
 
+
+
 (defn get-property-details [name]
+   [:div
+    {:class "bg-white"}
     [:div
+     {:class "relative isolate px-6 pt-14 lg:px-8"}
      [:div
-      {:class "px-4 sm:px-0"}
-      [:h3
-       {:class "text-base/7 font-semibold text-gray-900"}
-       "Property name:" " " name]
-        [:div
-       {:class "bg-white"}
-       [:div
-        {:class "relative isolate px-6 pt-14 lg:px-8"}
-        [:div
-         {:class "mx-auto max-w-2xl"}
-     [:div
- {:class "text-center"}
- [:h1 {:class "text-balance text-4xl font-bold tracking-tight text-gray-900 sm:text-6xl"} "Kalkulationen importieren"]
- [:div {:id "upload-file" :class "mt-10 flex items-center justify-center gap-x-6"}
-  [:form
-   {:hx-encoding "multipart/form-data"
-    :enctype "multipart/form-data"
-    :hx-post "/upload-property-details"
-    :hx-target "this"
-    :hx-swap "#upload-file"}
-   [:input {:type "file", :name "file"}]
-   [:button.inline-block.shrink-0.rounded-md.border.border-blue-600.bg-blue-600.px-12.py-3.text-sm.font-medium.text-white.transition.hover:bg-transparent.hover:text-blue-600.focus:outline-none.focus:ring.active:text-blue-500.dark:hover:bg-blue-700.dark:hover:text-white "Send"]]]]]
+      {:class "mx-auto max-w-2xl"}
       [:div
+       {:class "text-center"}
+        [:h3
+        {:class "text-base/7 font-semibold text-gray-900"}
+        "Property name:" " " name]
+       [:h1 {:class "text-balance text-4xl font-bold tracking-tight text-gray-900 sm:text-6xl"} "Kalkulationen importieren"]
+       [:div {:id "upload-file" :class "mt-10 flex items-center justify-center gap-x-6"}
+        [:form
+         {:hx-encoding "multipart/form-data"
+          :hx-post "/upload-property-details"
+          :hx-target "this"
+          :hx-swap "#upload-file"}
+         [:div
+          [:input {:type "file"
+                   :name "file"
+                   :id "property-bank-data"
+                   :class "hidden"
+                   :hx-on "change: this.form.requestSubmit();"}]
+          [:label {:for "property-bank-data"
+                   :id "upload-label-property"
+                   :class "cursor-pointer inline-block shrink-0 rounded-md border border-blue-600 bg-blue-600 px-12 py-3 text-sm font-medium text-white transition hover:bg-transparent hover:text-blue-600 focus:outline-none focus:ring active:text-blue-500 dark:hover:bg-blue-700 dark:hover:text-white"}
+           "Hochladen"]]]]]]
+     [:div
       {:class
-        "absolute inset-x-0 top-[calc(100%-13rem)] -z-10 transform-gpu overflow-hidden blur-3xl sm:top-[calc(100%-30rem)]",
-        :aria-hidden "true"}
-        [:div
-        {:class
-          "relative left-[calc(50%+3rem)] aspect-[1155/678] w-[36.125rem] -translate-x-1/2 bg-gradient-to-tr from-[#ff80b5] to-[#9089fc] opacity-30 sm:left-[calc(50%+36rem)] sm:w-[72.1875rem]",
-          :style
-          "clip-path: polygon(74.1% 44.1%, 100% 61.6%, 97.5% 26.9%, 85.5% 0.1%, 80.7% 2%, 72.5% 32.5%, 60.2% 62.4%, 52.4% 68.1%, 47.5% 58.3%, 45.2% 34.5%, 27.5% 76.7%, 0.1% 64.9%, 17.9% 100%, 27.6% 76.8%, 76.1% 97.7%, 74.1% 44.1%)"}]]]]]])
+       "absolute inset-x-0 top-[calc(100%-13rem)] -z-10 transform-gpu overflow-hidden blur-3xl sm:top-[calc(100%-30rem)]",
+       :aria-hidden "true"}
+      [:div
+       {:class
+        "relative left-[calc(50%+3rem)] aspect-[1155/678] w-[36.125rem] -translate-x-1/2 bg-gradient-to-tr from-[#ff80b5] to-[#9089fc] opacity-30 sm:left-[calc(50%+36rem)] sm:w-[72.1875rem]",
+        :style
+        "clip-path: polygon(74.1% 44.1%, 100% 61.6%, 97.5% 26.9%, 85.5% 0.1%, 80.7% 2%, 72.5% 32.5%, 60.2% 62.4%, 52.4% 68.1%, 47.5% 58.3%, 45.2% 34.5%, 27.5% 76.7%, 0.1% 64.9%, 17.9% 100%, 27.6% 76.8%, 76.1% 97.7%, 74.1% 44.1%)"}]]]])
