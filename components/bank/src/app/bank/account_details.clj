a434dd4e65af61a76abbe69aae55f8a831cb7f58
lgcanetti <lgcanetti@gmail.com>
2025-03-12 23:51:08 -0400

    Add file upload input

--- components/bank/src/app/bank/account_details.clj
+++ components/bank/src/app/bank/account_details.clj
@@ -3,6 +3,13 @@
 (defn content [id-account]
   [:div "Load content from database:" 
    [:p {:class "mb-4"} (str "Account details. ID: " id-account)]
+   [:div {:id "file-upload-container"}
+    [:input#upload_pdf {:type "file",
+             :accept "application/pdf",
+             :class "opacity-0 absolute"}];FIXME "hidden" Revisar esto
+    [:button {:_ "on click trigger click on #upload_pdf",
+             :class "bg-gray-500 hover:bg-gray-700 text-white font-bold py-1 px-2 rounded shadow-md focus:outline-none mb-4"}
+     "Upload PDF"]]
    [:a {
         :href "/bank",
         :class "bg-gray-500 hover:bg-gray-700 text-white font-bold py-1 px-2 rounded shadow-md focus:outline-none"} 
