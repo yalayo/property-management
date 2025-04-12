347b66e3861105c8501ad8e61059324992bfab9a
Yunior Alayo <yunior.alayo@gmail.com>
2025-04-12 12:47:54 +0200

    Removed custom domain resource temporarily.

--- projects/cloudflare/terraform/domain.tf
+++ projects/cloudflare/terraform/domain.tf
@@ -1,8 +1,8 @@
-resource "cloudflare_pages_domain" "custom_domain" {
-  account_id   = var.cloudflare_account_id
-  project_name = cloudflare_pages_project.pages_project.name
-  name         = "immo.busqandote.com"
-}
+# resource "cloudflare_pages_domain" "custom_domain" {
+#   account_id   = var.cloudflare_account_id
+#   project_name = cloudflare_pages_project.pages_project.name
+#   name         = "immo.busqandote.com"
+# }
 
 resource "cloudflare_dns_record" "dns_record" {
   zone_id = var.domain_zone_id
@@ -11,8 +11,4 @@ resource "cloudflare_dns_record" "dns_record" {
   proxied = true
   ttl = 1
   type = "CNAME"
-
-  depends_on = [
-    cloudflare_pages_domain.custom_domain
-  ]
 }
