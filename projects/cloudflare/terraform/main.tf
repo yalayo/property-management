d384f8db458fc8880e25d529bb0b855e75478461
Yunior Alayo <yunior.alayo@gmail.com>
2025-04-12 11:56:19 +0200

    Removed what i think is unnecessary.

--- projects/cloudflare/terraform/main.tf
+++ projects/cloudflare/terraform/main.tf
@@ -35,9 +35,4 @@ provider "cloudflare" {
 resource "cloudflare_pages_project" "pages_project" {
   account_id = var.cloudflare_account_id
   name       = "pmanagement-frontend"
-  production_branch = "main"
-  
-  source {
-    type = "manual"
-  }
 }
