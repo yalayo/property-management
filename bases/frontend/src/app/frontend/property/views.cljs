c40954069ccb0a009f9ba189b2c1c22641736f1d
Yunior Alayo <yunior.alayo@gmail.com>
2025-04-14 12:37:46 +0200

    Added properties to the component.

--- bases/frontend/src/app/frontend/property/views.cljs
+++ bases/frontend/src/app/frontend/property/views.cljs
@@ -11,4 +11,17 @@
   [property-list 
    {:id "properties"
     :propertyName @(re-frame/subscribe [::subs/form :property-name])
-    :onChangePropertyName #(re-frame/dispatch [::events/update-field :property-name (-> % .-target .-value)])}])
+    :propertyAddress @(re-frame/subscribe [::subs/form :property-address])
+    :propertyCity @(re-frame/subscribe [::subs/form :property-city])
+    :propertyPostalCode @(re-frame/subscribe [::subs/form :property-postal-code])
+    :propertyUnits @(re-frame/subscribe [::subs/form :property-units])
+    :propertyPurchasePrice @(re-frame/subscribe [::subs/form :property-purchase-price])
+    :propertyCurrentValue @(re-frame/subscribe [::subs/form :property-current-value])
+    :onChangePropertyName #(re-frame/dispatch [::events/update-field :property-name (-> % .-target .-value)])
+    :onChangePropertyAddress #(re-frame/dispatch [::events/update-field :property-address (-> % .-target .-value)])
+    :onChangePropertyCity #(re-frame/dispatch [::events/update-field :property-city (-> % .-target .-value)])
+    :onChangePropertyPostalCode #(re-frame/dispatch [::events/update-field :property-postal-code (-> % .-target .-value)])
+    :onChangePropertyUnits #(re-frame/dispatch [::events/update-field :property-units (-> % .-target .-value)])
+    :onChangePropertyPurchasePrice #(re-frame/dispatch [::events/update-field :property-purchase-price (-> % .-target .-value)])
+    :onChangePropertyCurrentValue #(re-frame/dispatch [::events/update-field :property-current-value (-> % .-target .-value)])
+    :submitProperty #(re-frame/dispatch [::events/save-property])}])
