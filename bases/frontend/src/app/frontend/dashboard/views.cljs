27e50a24cf025db617c75861193f100191a1ae92
Yunior Alayo <yunior.alayo@gmail.com>
2025-04-14 12:06:40 +0200

    Using the new properties component.

--- bases/frontend/src/app/frontend/dashboard/views.cljs
+++ bases/frontend/src/app/frontend/dashboard/views.cljs
@@ -1,12 +1,11 @@
 (ns app.frontend.dashboard.views
   (:require [reagent.core  :as r]
             [re-frame.core :as re-frame]
-            ["/pages/dashboard$default" :as dashboard-js]
-            ["/components/dashboard/PropertyList$default" :as property-list-js]))
+            [app.frontend.property.views :as property]
+            ["/pages/dashboard$default" :as dashboard-js]))
 
 (def dashboard (r/adapt-react-class dashboard-js))
-(def property-list (r/adapt-react-class property-list-js))
 
 (defn dashboard-component []
   [dashboard {:activeTab "properties"}
-   [property-list {:id "properties"}]])
+   (property/property-list-component)])
