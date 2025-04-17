d4152ee09d8aa9e9e66d4aa1c47e1285e1e46e58
Yunior Alayo <yunior.alayo@gmail.com>
2025-04-17 21:36:26 +0200

    Fixing the hiden component.

--- bases/frontend/ui/pages/dashboard.tsx
+++ bases/frontend/ui/pages/dashboard.tsx
@@ -24,7 +24,7 @@ export default function Dashboard(props) {
   return (
     <div className="min-h-screen bg-gray-50 flex">
       {/* Sidebar */}
-      <aside className="hidden md:flex md:w-64 md:flex-col md:fixed md:inset-y-0">
+      <aside className="md:flex md:w-64 md:flex-col md:fixed md:inset-y-0">
         <div className="flex-1 flex flex-col min-h-0 bg-white border-r border-gray-200">
           <div className="flex items-center h-16 flex-shrink-0 px-4 bg-primary-600">
             <Link href="/">
