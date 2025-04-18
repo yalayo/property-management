05c87ea97f0c8cc4647380e7f800cbca85659959
Yunior Alayo <yunior.alayo@gmail.com>
2025-04-18 17:45:04 +0200

    Created a folder to work with bank information.

---
+++ bases/frontend/src/app/frontend/bank/events.cljs
@@ -0,0 +1,77 @@
+(ns app.frontend.bank.events
+  (:require [re-frame.core :as re-frame :refer [after]]
+            [cljs.reader]
+            [app.frontend.config :as config]
+            [app.frontend.events :as main-events]
+            [day8.re-frame.http-fx]
+            [ajax.edn :as ajax-edn]))
+
+(def local-storage-interceptor main-events/->local-store)
+
+(re-frame/reg-event-db
+ ::update-field
+ [local-storage-interceptor]
+ (fn [db [_ id val]]
+   (assoc-in db [:property :form id] val)))
+
+(re-frame/reg-event-fx
+ ::upload-data
+ (fn [_ [_ file]]
+   {:http-xhrio {:method          :post
+                 :uri             (str config/api-url "/api/upload-transactions")
+                 :bod             (let [form-data (js/FormData.)]
+                                    (.append form-data "file" file)
+                                    form-data)
+                 :format          (ajax-edn/edn-request-format)
+                 :response-format (ajax-edn/edn-response-format)
+                 :timeout         8000
+                 :on-success      [:upload-success]
+                 :on-failure      [:upload-failure]}}))
+
+(re-frame/reg-event-db
+ ::upload-success
+ (fn [db [_ response]]
+   (js/console.log "File uploaded:" response)
+   (-> db
+       (assoc-in [:property :form] nil)
+       (assoc-in [:property ::add-propery-dialog-open] false))))
+
+(re-frame/reg-event-fx
+ ::upload-failure
+ (fn [{:keys [_]} [_ error]]
+   (js/console.error "Failed to upload file:" error)
+   {}))
+
+(re-frame/reg-event-fx
+ ::get-properties
+ (fn [{:keys [db]} _]
+   {:http-xhrio {:method          :get
+                 :uri             (str config/api-url "/api/properties")
+                 :response-format (ajax-edn/edn-response-format)
+                 :timeout         8000
+                 :on-success      [::update-db]
+                 :on-failure      [::get-properties-error]}}))
+
+(re-frame/reg-event-db
+ ::update-db
+ [local-storage-interceptor]
+ (fn [db [_ response]]
+   (assoc-in db [:property :properties] response)))
+
+(re-frame/reg-event-fx
+ ::get-properties-error
+ (fn [{:keys [_]} [_ error]]
+   (js/console.error "Failed to get the list of properties:" error)
+   {}))
+
+(re-frame/reg-event-db
+ ::show-add-property-dialog
+ [local-storage-interceptor]
+ (fn [db]
+   (assoc-in db [:property :add-propery-dialog-open] true)))
+
+(re-frame/reg-event-db
+ ::close-add-property-dialog
+ [local-storage-interceptor]
+ (fn [db]
+   (assoc-in db [:property :add-propery-dialog-open] false)))
---
+++ bases/frontend/src/app/frontend/bank/views.cljs
@@ -0,0 +1,33 @@
+(ns app.frontend.bank.views
+  (:require [reagent.core  :as r]
+            [re-frame.core :as re-frame]
+            [app.frontend.property.subs :as subs]
+            [app.frontend.property.events :as events]
+            ["/components/dashboard/PropertyList$default" :as property-list-js]))
+
+(def property-list (r/adapt-react-class property-list-js))
+
+(defn property-list-component []
+  (re-frame/dispatch [::events/get-properties]) 
+
+  [property-list 
+   {:id "properties"
+    :properties @(re-frame/subscribe [::subs/properties])
+    :isAddPropertyDialogOpen @(re-frame/subscribe [::subs/add-propery-dialog-open])
+    :propertyName @(re-frame/subscribe [::subs/form :name])
+    :propertyAddress @(re-frame/subscribe [::subs/form :address])
+    :propertyCity @(re-frame/subscribe [::subs/form :city])
+    :propertyPostalCode @(re-frame/subscribe [::subs/form :postalcode])
+    :propertyUnits @(re-frame/subscribe [::subs/form :units])
+    :propertyPurchasePrice @(re-frame/subscribe [::subs/form :purchaseprice])
+    :propertyCurrentValue @(re-frame/subscribe [::subs/form :currentvalue])
+    :onChangeAddPropertyDialogOpen #(re-frame/dispatch [::events/show-add-property-dialog])
+    :onChangeAddPropertyDialogClose #(re-frame/dispatch [::events/close-add-property-dialog])
+    :onChangePropertyName #(re-frame/dispatch [::events/update-field :name (-> % .-target .-value)])
+    :onChangePropertyAddress #(re-frame/dispatch [::events/update-field :address (-> % .-target .-value)])
+    :onChangePropertyCity #(re-frame/dispatch [::events/update-field :city (-> % .-target .-value)])
+    :onChangePropertyPostalCode #(re-frame/dispatch [::events/update-field :postalcode (-> % .-target .-value)])
+    :onChangePropertyUnits #(re-frame/dispatch [::events/update-field :units (-> % .-target .-value)])
+    :onChangePropertyPurchasePrice #(re-frame/dispatch [::events/update-field :purchaseprice (-> % .-target .-value)])
+    :onChangePropertyCurrentValue #(re-frame/dispatch [::events/update-field :currentvalue (-> % .-target .-value)])
+    :submitProperty #(re-frame/dispatch [::events/save-property])}])
