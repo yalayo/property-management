f5b601fb9a34814ff95483521eb2c3b48a27638b
Yunior Alayo <yunior.alayo@gmail.com>
2025-04-18 12:16:37 +0200

    Removed all test code.

--- bases/frontend/src/app/frontend/views.cljs
+++ bases/frontend/src/app/frontend/views.cljs
@@ -1,39 +1,6 @@
 (ns app.frontend.views
-  (:require [reagent.core  :as r]
-            [re-frame.core :as re-frame]
-            [app.frontend.events :as events]
-            [app.frontend.subs :as subs]
-            [app.frontend.user.views :as user-view]
-            ["/pages/home$default" :as home]
-            [app.frontend.landing.views :as landing]
-            [app.frontend.survey.views :as survey]
-            [app.frontend.dashboard.views :as dashboard]))
-
-(def home-component (r/adapt-react-class home))
-
-;; Testing the login component
-#_(defn app []
-  [:<>
-   [user-view/login-component]])
-
-#_(defn app []
-  [:<>
-   [home-component {:isLoggedIn false :user {}}
-    (survey/survey-component)]])
-
-;; Testing the dashboard component
-#_(defn app []
-  [:<>
-   (dashboard/dashboard-component)])
-
-;; Testing the landing page
-#_(defn app []
-  [:<>
-   (landing/landing-component)])
+  (:require [app.frontend.landing.views :as landing]))
 
 (defn app []
-  (let [user-loged-in? @(re-frame/subscribe [::subs/logged-in])]
-    [:<>
-     (if user-loged-in?
-       (dashboard/dashboard-component)
-       (user-view/login-component))]))
+  [:<>
+   (landing/landing-component)])
