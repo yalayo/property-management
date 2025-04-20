2e50a443eb157fb1d86858253427fca6f90ace69
Yunior Alayo <yunior.alayo@gmail.com>
2025-04-20 17:37:54 +0200

    Fixing small ui problem.

--- bases/frontend/ui/components/dashboard/DashboardSummary.tsx
+++ bases/frontend/ui/components/dashboard/DashboardSummary.tsx
@@ -76,7 +76,7 @@ export default function DashboardSummary(props) {
           ))}
         </div>
       ) : (
-        <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-4 gap-4">
+        <div className="grid sm:grid-cols-2 lg:grid-cols-4 gap-4">
           <Card>
             <CardHeader className="flex flex-row items-center justify-between space-y-0 pb-2">
               <CardTitle className="text-sm font-medium">Properties</CardTitle>
