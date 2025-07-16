cdc8838dbf77982b956d53230e8e9aca0d81f1d8
Yunior <yunior.alayo@gmail.com>
2025-07-16 19:00:07 +0200

    Updated the storage component to not use datasource.

--- components/storage/src/app/storage/core.clj
+++ components/storage/src/app/storage/core.clj
@@ -1,17 +1,12 @@
 (ns app.storage.core
-  (:require [next.jdbc.connection :as connection])
-  (:import (com.zaxxer.hikari HikariDataSource)
-           (org.flywaydb.core Flyway)))
+  (:require [com.stuartsierra.component :as component]))
 
-(defn datasource-component [config]
-  (connection/component 
-   HikariDataSource
-   (assoc (:db-spec config)
-          :init-fn (fn [datasource]
-                     (println "Running database init")
-                     (.migrate
-                      (.. (Flyway/configure)
-                          (dataSource datasource)
-                          (locations (into-array String ["classpath:migrations"]))
-                          (table "schema_version")
-                          (load)))))))
+(defrecord StorageComponent [config]
+  component/Lifecycle
+
+  (start [component])
+
+  (stop [component]))
+
+(defn storage-component [config]
+  (map->StorageComponent config))
