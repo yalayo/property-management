1efff74808d699fd338b1b37403aecae8c06f198
Yunior <yunior.alayo@gmail.com>
2025-07-16 19:02:05 +0200

    Updated the system.

--- development/src/system.clj
+++ development/src/system.clj
@@ -1,7 +1,6 @@
 (ns system
   (:require [com.stuartsierra.component :as component]
             [com.brunobonacci.mulog :as mu]
-            [app.storage.interface :as storage]
             [app.route.interface :as route]
             [app.server.core :as server]
             [app.flags.interface :as flags]
@@ -47,7 +46,6 @@
 ;; Load all necessary data when the system is being created
 (defn create-system [config]
   (component/system-map
-   :datasource (storage/datasource-component config)
    :user (user/user-component config)
    :survey (survey/survey-component config)
    :property (property/property-component config)
@@ -58,10 +56,10 @@
    :routes (route/route-component {:config (:routes config)})
    :server (component/using
             (server/server-component {:port 8080 :active-route :external})
-            [:datasource :routes])
+            [:routes])
    :internal-server (component/using
             (server/server-component {:port 9090 :active-route :internal})
-            [:datasource :routes])))
+            [:routes])))
 
 (defn start []
   (init-logging)
