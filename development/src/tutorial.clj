5d951f6219edf71efacabc9c63e89b38671c9e0f
Yunior <yunior.alayo@gmail.com>
2025-08-17 21:25:54 +0200

    Added a file for a clojure tutorial.

---
+++ development/src/tutorial.clj
@@ -0,0 +1,12 @@
+(ns tutorial)
+
+;; This is a comment
+
+;; This is how you declare variables
+(def integer 10)
+
+;; Now let's see the value of that variable
+integer
+
+;; Everything is declared between parentesis
+;; Now let's declare a 
