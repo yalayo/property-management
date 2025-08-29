268c66e3efe5d39dea74b7275a9e32a84d819868
Yunior <yunior.alayo@gmail.com>
2025-08-29 08:50:02 +0200

    Added database related env variables.

--- projects/main/shell.nix
+++ projects/main/shell.nix
@@ -9,6 +9,12 @@ pkgs.mkShell {
   ];
 
   shellHook = ''
+    export JAVA_OPTS="-Xms512m -Xmx2g"
+    echo "🚀 Dev shell ready with JAVA_OPTS=$JAVA_OPTS"
+
+    export DB_PASSWORD="$POSTGRES_PASSWORD"
+    export DB_HOST="$POSTGRES_HOST"
+
     echo "🚀 Clojure dev shell ready."
     echo "Run: clojure -M -m app.main.core"
     echo "Or start a REPL with: clojure -M:repl"
