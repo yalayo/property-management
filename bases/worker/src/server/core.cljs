c90a0e5df8a5012fc0957d84ee4b1cfb15580917
Yunior <yunior.alayo@gmail.com>
2025-09-11 17:00:14 +0200

    Now working without multimethod.

--- bases/worker/src/server/core.cljs
+++ bases/worker/src/server/core.cljs
@@ -1,25 +1,34 @@
 (ns server.core
-  (:require [reitit.core :as r]
-            [clojure.string :as s]
-            [server.cf :as cf]
-            [app.user.interface :as user]))
+  (:require ["cloudflare:workers" :refer [DurableObject]]
+            [reitit.core :as r]
+         	[clojure.string :as s]
+            [lib.async :refer [js-await]]
+            [server.cf.durable-objects :as do]
+            [server.cf :as cf :refer [defclass]]))
 
-(def base-routes
-  ["/api"])
+;; usage example of Durable Objects as a short-lived state
+;; for user presence tracking in multiplayer web app
+(defclass ^{:extends DurableObject} PresenceDurableObject [ctx env]
+  Object
+  (constructor [this ctx env]
+               (super ctx env))
 
-#_(def routes
-  (into base-routes (user/get-routes) #_(concat (excel/routes) (user/routes))))
+  (add-user-presence+ [this id timestamp]
+                      (js-await [_ (do/storage-put+ ctx id timestamp)
+                                 users (do/storage-list+ ctx)
+                                 now (js/Date.now)]
+                                (doseq [[id _] (->> (cf/js->clj users)
+                                                    (filter (fn [[id ts]] (> (- now ts) 10000))))]
+                                  (do/storage-delete+ ctx id))
+                                (do/storage-list+ ctx))))
 
 (defn todos-get [route request env ctx]
   (cf/response-edn {:result :hello} {:status 200}))
 
-
-;; Define routes correctly with nested vectors
-(def routes
-  ["/api" ["/todos" {:name ::todos :get  todos-get}]])
-
 (def router
-  (r/router [routes])) ;; important: wrap in a vector of top-level routes
+  (r/router
+   ["/api"
+    ["/todos" {:name ::todos :get  todos-get}]]))
 
 (defn handle-route [route request env ctx]
   (let [method (keyword (s/lower-case (.-method ^js request)))
