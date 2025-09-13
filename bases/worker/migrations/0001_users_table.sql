13e85a031d2584fb14fa2aa2c74e0c6bc9e97168
Yunior <yunior.alayo@gmail.com>
2025-09-13 10:20:22 +0200

    Update the migration.

--- bases/worker/migrations/0001_users_table.sql
+++ bases/worker/migrations/0001_users_table.sql
@@ -1,7 +1,8 @@
 -- Migration number: 0001 	 2025-09-11T15:34:40.031Z
 -- Table to store user accounts
 CREATE TABLE accounts (
-    email TEXT PRIMARY KEY,
+    user_id TEXT PRIMARY KEY,
+    email TEXT NOT NULL UNIQUE,
     password TEXT NOT NULL,
     created_at TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP
 );
