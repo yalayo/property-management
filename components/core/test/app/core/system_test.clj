be21f6c8b311e657e954832235f468dc317c8c06
Yunior <yunior.alayo@gmail.com>
2025-09-19 10:23:17 +0200

    Resusing property based test.

--- components/core/test/app/core/system_test.clj
+++ components/core/test/app/core/system_test.clj
@@ -1,21 +1,10 @@
 (ns app.core.system-test
   (:require [clojure.test :refer :all]
-             [fugato.core :as f]
-             [clojure.test.check.generators :as gen]
-             [odoyle.rules :as o]
-             [hausverwaltung.rules :as hr]
-             [hausverwaltung.generators :as g]))
+            [clojure.test.check.clojure-test :refer [defspec]]
+            [clojure.test.check.properties :as prop]
+            [fugato.core :as fugato]
+            [app.core.fugato :as f]))
   
-  (deftest late-payment-detected
-    (f/defcheck rent-check
-      [contracts (gen/vector g/gen-contract 3)
-       payments  (gen/vector g/gen-payment 2)] ;; fewer payments on purpose
-      (let [session (-> (hr/make-session)
-                        (as-> s
-                              (reduce (fn [s c] (o/insert s (:contract/id c) c)) s contracts)
-                          (reduce (fn [s p] (o/insert s (:payment/id p) p)) s payments)
-                          (o/fire-rules)))]
-        ;; property: at least one tenant may be marked late if missing a payment
-        (let [statuses (map #(o/query-all session %)
-                            (map :contract/tenant contracts))]
-          (is (every? some? statuses))))))
+(defspec model-eq-reality 10
+  (prop/for-all [commands (fugato/commands f/model f/initial-state 10 1)]
+                (= (f/run f/initial-state commands) (-> commands last meta :after))))
