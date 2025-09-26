1ac2ec1ec7e211cb2fb26f5ae44baadb4e1d1f32
Yunior <yunior.alayo@gmail.com>
2025-09-26 16:20:41 +0200

    Small update.

--- development/src/system.clj
+++ development/src/system.clj
@@ -18,7 +18,7 @@
 (def state (atom nil))
 
 (def config {:db-spec {:dbtype "postgres"
-                       :host (if (= (System/getenv "ENVIRONMENT") "prod") (System/getenv "DB_HOST") "localhost")
+                       :host (System/getenv "DB_HOST")
                        :dbname "property-management"
                        :username "user"
                        :password (System/getenv "DB_PASSWORD")
