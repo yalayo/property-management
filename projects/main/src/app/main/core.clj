c7c8f7a7882c86aba816e66239401aab78f2e024
Yunior <yunior.alayo@gmail.com>
2025-09-26 16:37:37 +0200

    Adapted to work with integrant.

--- projects/main/src/app/main/core.clj
+++ projects/main/src/app/main/core.clj
@@ -1,28 +1,47 @@
 (ns app.main.core
-  (:require [com.stuartsierra.component :as component]
-            [com.brunobonacci.mulog :as mu]
-            [app.route.interface :as route]
-            [app.server.core :as server]
-            [app.flags.interface :as flags]
-            [app.property.interface :as property]
-            [app.tenant.interface :as tenant]
-            [app.apartment.interface :as apartment]
-            [app.account.interface :as account]
-            [app.operations.interface :as operations]
-            [app.bank.interface :as bank]
-            [app.html.interface :as html]
-            [app.user.interface :as user]
-            [app.survey.interface :as survey]))
+  (:require
+   [integrant.core :as ig]
+   [com.brunobonacci.mulog :as mu]
+   [app.route.interface :as route]
+   [app.server.core :as server]
+   [app.flags.interface :as flags]
+   [app.property.interface :as property]
+   [app.tenant.interface :as tenant]
+   [app.apartment.interface :as apartment]
+   [app.account.interface :as account]
+   [app.operations.interface :as operations]
+   [app.bank.interface :as bank]
+   [app.html.interface :as html]
+   [app.user.interface :as user]
+   [app.survey.interface :as survey]
+   [app.storage.interface :as storage]
+   [app.core.interface :as core]))
 
-(def config {:db-spec {:dbtype "postgres"
-                       :host (System/getenv "DB_HOST")
-                       :dbname "property-management"
-                       :username "user"
-                       :password (System/getenv "DB_PASSWORD")
-                       :dataSourceProperties {:socketTimeout 30}}
-             :routes {:external (into #{} (concat (user/get-routes) (html/get-routes) (property/get-routes) (tenant/get-routes) (apartment/get-routes) (account/get-routes) (bank/get-routes) (survey/get-routes) (operations/get-routes)))
-                      :internal (into #{} (concat (user/get-internal-routes) (flags/get-routes) (property/get-internal-routes) 
-                                                  (bank/get-internal-routes)))}})
+(def base-config
+  {:routes
+   {:external (into #{} (concat (user/get-routes)
+                                (html/get-routes)
+                                (property/get-routes)
+                                (tenant/get-routes)
+                                (apartment/get-routes)
+                                (account/get-routes)
+                                (bank/get-routes)
+                                (survey/get-routes)
+                                (operations/get-routes)))
+    :internal (into #{} (concat (user/get-internal-routes)
+                                (flags/get-routes)
+                                (property/get-internal-routes)
+                                (bank/get-internal-routes)))
+    :schema (user/get-schema)}})
+
+(def config
+  {::core/domain {:initial {}}
+   ::storage/storage {:database-name "users" :schema (:schema base-config)}
+   ::user/routes {:shell nil :core (ig/ref ::core/domain)}
+   ::route/external-routes {:routes (get-in base-config [:routes :external])}
+   ::route/internal-routes {:routes (get-in base-config [:routes :internal])}
+   ::server/server {:port 8080 :active-route :external :routes (ig/ref ::route/external-routes)}
+   ::server/internal-server {:port 9090 :active-route :internal :routes (ig/ref ::route/internal-routes)}})
 
 (defn init-logging []
   (let [prod (System/getenv "ENVIRONMENT")
@@ -38,28 +57,21 @@
                  (assoc data :publishers publishers))]
     (mu/start-publisher! config)))
 
-(defn create-system [config]
-  (component/system-map
-   :user (user/user-component config)
-   :survey (survey/survey-component config)
-   :property (property/property-component config)
-   :tenant (tenant/tenant-component config)
-   :apartment (apartment/apartment-component config)
-   :account (account/account-component config)
-   :operations (operations/operations-component config)
-   :routes (route/route-component {:config (:routes config)})
-   :server (component/using
-            (server/server-component {:port 8080 :active-route :external})
-            [:routes])
-   :internal-server (component/using
-            (server/server-component {:port 9090 :active-route :internal})
-            [:routes])))
+(defonce system (atom nil))
+
+(defn start []
+  (reset! system (ig/init config)))
+
+(defn stop []
+  (when @system
+    (ig/halt! @system)
+    (reset! system nil)))
+
+(defn restart []
+  (stop)
+  (start))
+
 
 (defn -main []
   (init-logging)
-  (let [system (-> config
-                   (create-system)
-                   (component/start-system))]
-    (.addShutdownHook
-     (Runtime/getRuntime)
-     (new Thread #(component/stop-system system)))))
+  (start))
