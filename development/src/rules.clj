eae007c1806908241336a64e3abdda143052df4a
Yunior <yunior.alayo@gmail.com>
2025-09-27 21:37:21 +0200

    Experimenting with user sign-up.

--- development/src/rules.clj
+++ development/src/rules.clj
@@ -4,28 +4,68 @@
 
 (def rules
   (o/ruleset
-   {::move-player
+   {::users
     [:what
-     [::time ::total tt]
-     :then
-     (o/insert! ::player ::x tt)]
-
-    ::player
+     #_[id ::user user-id]
+     [id ::user email]
+     [id ::name name]
+     :when (not= id ::sign-up)]
+    
+    ::user-sign-up
     [:what
-     [::player ::x x]
-     [::player ::y y]]}))
+     [::sign-up ::id id]
+     [::sign-up ::user email]
+     [::sign-up ::name name]
+     #_#_:then
+     (do
+       #_(o/insert! ::users ::user user-id)
+       (o/insert! id ::user email)
+       (o/insert! id ::name name))]
+    
+    ::duplicate-email
+    [:what
+     [::sign-up ::user email]
+     [id ::user email]
+     :when (not= id ::sign-up)
+     #_#_:then
+     (o/insert! ::duplicate-email ::present true)]}))
 
 ;; create session and add rule
 (def *session
   (atom (reduce o/add-rule (o/->session) rules)))
 
+#_(def *session
+  (atom (o/add-rule (o/->session) rules)))
+
 (comment
+  "First attempt to sign-up and if there is no duplication then send another insert user"
+
   (swap! *session
          (fn [session]
            (-> session
-               (o/insert ::player ::x 20)
-               (o/insert ::player ::y 15)
+               (o/insert :user-1 ::user "user-1@mail.com")
+               (o/insert :user-1 ::name "User 1")
                o/fire-rules)))
   
-  (o/query-all @*session ::player)
+  (swap! *session
+         (fn [session]
+           (-> session
+               (o/insert :user-2 ::user "user-2@mail.com")
+               (o/insert :user-2 ::name "User 2")
+               o/fire-rules)))
+  
+  (do (swap! *session
+         (fn [session]
+           (-> session
+               (o/insert ::sign-up ::id :user-1)
+               (o/insert ::sign-up ::name "User 1")
+               (o/insert ::sign-up ::user "user-1@mail.com")
+               o/fire-rules))) nil)
+  
+  
+  (o/query-all @*session ::users)
+
+  (o/query-all @*session ::duplicate-email)
+
+  (o/query-all @*session ::user-sign-up)
   )
