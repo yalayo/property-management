189bcbcddf2205bed7841e8bea910a5c481ec470
Yunior <yunior.alayo@gmail.com>
2025-10-21 21:09:57 +0200

    Update the tutorial code.

--- development/src/elevator.clj
+++ development/src/elevator.clj
@@ -17,7 +17,7 @@ texto
   (println "toilet")
   (println "skibidi"))
 
- (println "skibidi" texto)
+(println "skibidi" texto)
 
 (def cinco 5)
 cinco
@@ -34,9 +34,9 @@ ex
 (- cinco mil ex)
 
 (- mil cinco  ex)
- 
+
 (* cinco mil)
- 
+
 (def lista [1 2 3 4 0])
 
 lista
@@ -47,9 +47,8 @@ lista
 (nth lista 0)
 
 
-(def elevator 
-  {
-   :floor 0
+(def elevator
+  {:floor 0
    :moving false
    :door_status :close})
 
@@ -58,5 +57,15 @@ elevator
 (defn move [floor]
   (assoc elevator :floor floor))
 
-(move 3)
+#_(move 3)
 
+(defn call [floor] 
+  (let [door_status (:door_status elevator)
+        current_floor (:floor elevator) ]
+    (when (= door_status :open)
+      (assoc elevator :door_status :close))
+    (println elevator)))
+
+(call 3)
+
+(:door_status elevator)
