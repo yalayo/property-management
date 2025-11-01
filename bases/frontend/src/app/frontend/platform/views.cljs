965457aa52f2ba70939422e761d0dc67d89736b8
Yunior <yunior.alayo@gmail.com>
2025-11-01 20:16:41 +0100

    Updated to show the platform direct on the internal.

--- bases/frontend/src/app/frontend/platform/views.cljs
+++ bases/frontend/src/app/frontend/platform/views.cljs
@@ -1,6 +1,8 @@
 (ns app.frontend.platform.views
   (:require [re-frame.core :as re-frame]
             [reagent.core :as r]
+            [clojure.string :as str]
+            [app.frontend.config :as config]
             [app.frontend.platform.subs :as subs]
             [app.frontend.user.views :as user-view]
             [app.frontend.dashboard.views :as dashboard]))
@@ -14,9 +16,14 @@
        [:h1 "Login with Google"]
        [:div#g-signin]])}))
 
+(defn internal? []
+  (str/includes? (.-host js/window.location) "localhost:9090"))
+
 (defn app []
   (let [user-loged-in? @(re-frame/subscribe [::subs/logged-in])]
     [:<>
-     (if user-loged-in?
+     (if (internal?)
        (dashboard/dashboard-component)
-       (user-view/access-component))]))
+       (if user-loged-in?
+         (dashboard/dashboard-component)
+         (user-view/access-component)))]))
