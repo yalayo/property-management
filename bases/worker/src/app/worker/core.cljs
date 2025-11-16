fded79b2680c97943ff7cb48ac30caaf5aa3c915
Yunior <yunior.alayo@gmail.com>
2025-11-16 13:10:05 +0100

    Updated to support CORS.

--- bases/worker/src/app/worker/core.cljs
+++ bases/worker/src/app/worker/core.cljs
@@ -23,15 +23,54 @@
                                   (do/storage-delete+ ctx id))
                                 (do/storage-list+ ctx))))
 
+(def allowed-origins
+  #{"http://localhost:8081"
+    "https://anudis.com"
+    "https://www.anudis.com"})
+
+(defn cors-headers-for [origin]
+  (if (allowed-origins origin)
+    {"Access-Control-Allow-Origin" origin
+     "Vary" "Origin"
+     "Access-Control-Allow-Methods" "GET, POST, PUT, PATCH, DELETE, OPTIONS"
+     "Access-Control-Allow-Headers" "Content-Type, Authorization"
+     "Access-Control-Max-Age" "86400"}
+    ;; origin not allowed
+    {"Access-Control-Allow-Origin" "null"
+     "Vary" "Origin"}))
+
+(defn ensure-js-response [resp]
+  (if (instance? js/Response resp)
+    resp
+    (let [{:keys [status headers body]} resp
+          init #js {:status (or status 200)
+                    :headers (clj->js headers)}]
+      ;; Important: pass body *only if NOT nil*
+      (if (nil? body)
+        (js/Response. nil init)
+        (js/Response. body init)))))
+
+(defn add-cors-response [resp origin]
+  (let [resp (ensure-js-response resp)
+        hdrs (.-headers resp)]
+    (doseq [[k v] (cors-headers-for origin)]
+      (.set hdrs k v))
+    resp))
+
 (def base-routes
   ["/api"])
 
 (defn handle-route [route request env ctx]
-  (let [method (keyword (s/lower-case (.-method ^js request)))
-        handler (get-in route [:data method])]
-    (if handler
-      (handler route request env ctx)
-      (cf/response-error {:error "Not implemented"}))))
+  (let [origin (.get (.-headers request) "Origin")
+        method (.-method request)]
+    (println "Method: " method)
+    (if (= method "OPTIONS") ;; Preflight
+      (add-cors-response (cf/response nil {:status 204}) origin)
+      (let [method-k (keyword (.toLowerCase method))
+            handler   (get-in route [:data method-k])]
+        (if handler
+          (add-cors-response (handler route request env ctx) origin)
+          (add-cors-response (cf/response-error {:error "Not implemented"}) origin))))))
 
 (defn init [{:keys [upload-routes]}]
   (let [routes (into base-routes upload-routes)
