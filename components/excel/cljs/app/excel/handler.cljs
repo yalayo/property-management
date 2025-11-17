ea1a437d30bd9eec54706fb3a8024cf6bfa54c10
Yunior <yunior.alayo@gmail.com>
2025-11-17 17:27:56 +0100

    Working on the excel file processing.

--- components/excel/cljs/app/excel/handler.cljs
+++ components/excel/cljs/app/excel/handler.cljs
@@ -4,10 +4,46 @@
             [app.worker.async :refer [js-await]]))
 
 (defn post-upload-details [route request env ctx]
-  (println "Request: " request)
   (js-await [form-data (.formData request)
-             file (.get form-data "file")
-             buf (.arrayBuffer file)
-             result (core/process buf)]
-            (println "Uploading ...")
-            (cf/response-edn {:result result} {:status 200})))
+             file      (.get form-data "file")
+             email     (.get form-data "email")
+             buf       (.arrayBuffer file)
+             result    (core/process buf)]
+            (println "Uploading ... file name:" (.-name file) ", email:" email)
+            (cf/response-edn {:result result
+                              :email  email
+                              :file   (.-name file)}
+                             {:status 200})))
+
+#_(defn iterator->seq [it]
+  (when-some [n (.next it)]
+    (when-not (.-done n)
+      (cons (.-value n)
+            (lazy-seq (iterator->seq it))))))
+
+
+
+
+#_(defn post-upload-details [route request env ctx]
+  ;; Print the request metadata (URL, headers, etc.)
+  (println "Request:" (.-url request))
+  (println "Headers:")
+  (doseq [[k v] (js->clj (.-headers request))]
+    (println " " k ":" v))
+
+  ;; Now inspect multipart fields
+  (js-await [form-data (.formData request)]
+            (println "Form parts:")
+  
+            (doseq [[k v] (iterator->seq (.entries form-data))]
+              (cond
+                ;; File-like?
+                (and (object? v) (.-arrayBuffer v))
+                (println " " k "-> FILE" {:name (.-name v)
+                                          :size (.-size v)
+                                          :type (.-type v)})
+  
+                :else
+                (println " " k "->" v)))
+  
+            (cf/response-edn {:ok true} {:status 200})))
