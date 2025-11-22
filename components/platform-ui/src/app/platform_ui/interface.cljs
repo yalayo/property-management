fc91cf155e6ac0a89fbf57a14eea2ae49f00bfcb
Yunior <yunior.alayo@gmail.com>
2025-11-22 16:12:11 +0100

    Fixing some issues on the platform ui component.

--- components/platform-ui/src/app/platform_ui/interface.cljs
+++ components/platform-ui/src/app/platform_ui/interface.cljs
@@ -2,5 +2,5 @@
   (:require [integrant.core :as ig]
             [app.platform-ui.views :as views]))
 
-(defmethod ig/init-key ::component [_ {:keys []}]
-  views/file-upload-component)
+(defmethod ig/init-key ::component [_ {:keys [user-component]}]
+  views/platform-component user-component)
--- components/platform-ui/src/app/platform_ui/views.cljs
+++ components/platform-ui/src/app/platform_ui/views.cljs
@@ -1,16 +1,13 @@
 (ns app.platform-ui.views
   (:require [re-frame.core :as re-frame]
-            [reagent.core :as r]
             [app.frontend.config :as config]
-            [app.frontend.platform.subs :as subs]
-            [app.frontend.user.views :as user-view]
-            [app.frontend.dashboard.views :as dashboard]))
+            [app.platform-ui.subs :as subs]))
 
-(defn app []
+(defn platform-component [user-component]
   (let [user-loged-in? @(re-frame/subscribe [::subs/logged-in])]
     [:<>
      (if (config/internal?)
-       (dashboard/dashboard-component)
+       [:div "Temp dashboard place holder"]
        (if user-loged-in?
-         (dashboard/dashboard-component)
-         (user-view/access-component)))]))
+         [:div "Temp dashboard place holder"]
+         user-component))]))
