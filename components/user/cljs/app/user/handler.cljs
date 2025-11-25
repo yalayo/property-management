86ff688077e1f4238feb7b22c1e0f11b6572ea89
Yunior <yunior.alayo@gmail.com>
2025-11-25 21:00:48 +0100

    Small paramenters changes.

--- components/user/cljs/app/user/handler.cljs
+++ components/user/cljs/app/user/handler.cljs
@@ -4,11 +4,11 @@
    [app.worker.async :refer [js-await]]
    [app.worker.cf :as cf]))
 
-(defn post-sign-in [route request env ctx]
+(defn post-sign-in [_ request _ _]
   (js-await [data (cf/request->edn request)]
             (println "Register: " data)))
 
-(defn post-sign-up [route request env ctx]
+(defn post-sign-up [_ request _ _]
   (let [data (cf/request->edn request)
         email (:email data)
         password (:password data)]
