b051f9e418f03852b1508722cbaa538b5b1ef739
Yunior <yunior.alayo@gmail.com>
2025-12-05 09:31:54 +0100

    Deploy to test.

--- bases/worker/src/app/worker/db.cljs
+++ bases/worker/src/app/worker/db.cljs
@@ -75,14 +75,15 @@
     (js-await [result (.run ^js (.apply bounded stmt (clj->js args)))]
               result)))
 
-(defn ^js/Promise run+ [env query]
-  (let [[sql & params] (sql/format query)
-        arr #js ["user-2" "user-1@mail.com" "password2"]
-        stmt (.prepare ^js (.-DB env) sql)]
-    (js-await [result (-> stmt
-                          (.bind (aget arr 0) (aget arr 1) (aget arr 2))
-                          (.run))]
-              (js->clj result :keywordize-keys true))))
+(defn ^:export run+ [^js env query]
+  (js/async
+   (fn []
+     (let [[sql & params] (sql/format query)
+           arr #js ["user-2" "user-1@mail.com" "password2"]
+           stmt (.prepare (.-DB env) sql)]
+       (js/await (-> stmt
+                     (.bind (aget arr 0) (aget arr 1) (aget arr 2))
+                     (.run)))))))
 
 #_(defn ^js/Promise run+ [query]
   (let [[sql & args] (sql/format query)
